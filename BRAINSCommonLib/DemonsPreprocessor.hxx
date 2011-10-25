BRAINSia/BRAINSStandAlone

STYLE: Push initialization to headers, remove cruft

BRAINSCommonLib/DemonsPreprocessor.hxx
@@ -19,29 +19,25 @@ namespace itk
 {
 template <typename TInputImage, typename TOutputImage>
 DemonsPreprocessor<TInputImage, TOutputImage>
-::DemonsPreprocessor()
+::DemonsPreprocessor() :
+  m_NumberOfHistogramLevels(256),
+  m_NumberOfMatchPoints(1),
+  m_FixedImageMinimum(NumericTraits<InputPixelType>::NonpositiveMin()),
+  m_MovingImageMinimum(NumericTraits<InputPixelType>::NonpositiveMin()),
+  m_FixedBinaryVolume("none"),
+  m_MovingBinaryVolume("none"),
+  m_Lower(NumericTraits<PixelType>::NonpositiveMin()),
+  m_Upper(NumericTraits<PixelType>::max()),
+  m_DefaultPixelValue(NumericTraits<PixelType>::One),
+  m_OutDebug(false),
+  m_UseHistogramMatching(0)
 {
-  m_UseHistogramMatching = 0;
-  m_NumberOfHistogramLevels = 256;
-  m_NumberOfMatchPoints = 1;
-
-  m_FixedImageMinimum =  NumericTraits<InputPixelType>::NonpositiveMin();
-  m_MovingImageMinimum = NumericTraits<InputPixelType>::NonpositiveMin();
-
-  m_FixedBinaryVolume = "none";
-  m_MovingBinaryVolume = "none";
-  //    m_Seed =  NumericTraits<IndexType>::Zero;
   for( unsigned i = 0; i < TInputImage::ImageDimension; ++i )
     {
     m_Seed[i] = 0;
     m_MedianFilterSize[i] = 0;
     }
-  m_Lower = NumericTraits<PixelType>::NonpositiveMin();
-  m_Upper = NumericTraits<PixelType>::max();
-
-  m_DefaultPixelValue = NumericTraits<PixelType>::One;
   m_Radius.Fill(1);
-  m_OutDebug = false;
 }
 
 template <typename TInputImage, typename TOutputImage>
@@ -111,7 +107,6 @@ DemonsPreprocessor<TInputImage, TOutputImage>
     histogramfilter->SetNumberOfMatchPoints(m_NumberOfMatchPoints);
     histogramfilter->ThresholdAtMeanIntensityOn();
     histogramfilter->Update();
-    //      m_OutputFixedImage  = histogramfilter->GetOutput();
     m_OutputMovingImage  = histogramfilter->GetOutput();
     // +DANGER: ALIASING:  m_OutputMovingImage EQ m_UnNormalizedMovingImage by
     // design.
@@ -119,9 +114,6 @@ DemonsPreprocessor<TInputImage, TOutputImage>
     // m_OutputMovingImage =
     // itkUtil::CopyImage<TOutputImage>(m_UnNormalizedMovingImage);
     }
-
-  //  m_OutputMovingImage =
-  // itkUtil::CopyImage<TOutputImage>(m_UnNormalizedMovingImage);
 
   m_OutputFixedImage = itkUtil::CopyImage<TOutputImage>(
       m_UnNormalizedFixedImage);

BRAINSCommonLib/itkOtsuHistogramMatchingImageFilter.hxx
@@ -29,30 +29,25 @@ namespace itk
   */
 template <class TInputImage, class TOutputImage, class THistogramMeasurement>
 OtsuHistogramMatchingImageFilter<TInputImage, TOutputImage, THistogramMeasurement>
-::OtsuHistogramMatchingImageFilter()
+::OtsuHistogramMatchingImageFilter() :
+  m_NumberOfHistogramLevels(256),
+  m_NumberOfMatchPoints(1),
+  m_SourceMinValue(0),
+  m_ReferenceMinValue(0),
+  m_SourceHistogram(HistogramType::New()),
+  m_ReferenceHistogram(HistogramType::New()),
+  m_OutputHistogram(HistogramType::New()),
+  m_LowerGradient(0.0),
+  m_UpperGradient(0.0),
+  m_SourceMask(NULL),
+  m_ReferenceMask(NULL)
 {
   this->SetNumberOfRequiredInputs(2);
-
-  m_NumberOfHistogramLevels = 256;
-  m_NumberOfMatchPoints = 1;
 
   m_QuantileTable.set_size(3, m_NumberOfMatchPoints + 2);
   m_QuantileTable.fill(0);
   m_Gradients.set_size(m_NumberOfMatchPoints + 1);
   m_Gradients.fill(0);
-
-  m_LowerGradient = 0.0;
-  m_UpperGradient = 0.0;
-
-  // Create histograms.
-  m_SourceHistogram = HistogramType::New();
-  m_ReferenceHistogram = HistogramType::New();
-  m_OutputHistogram = HistogramType::New();
-
-  m_SourceMinValue = 0;
-  m_ReferenceMinValue = 0;
-  m_SourceMask = NULL;
-  m_ReferenceMask = NULL;
 }
 
 /*

BRAINSia/BRAINSTools

BRAINSCommonLib/DemonsPreprocessor.hxx
@@ -19,29 +19,25 @@ namespace itk
 {
 template <typename TInputImage, typename TOutputImage>
 DemonsPreprocessor<TInputImage, TOutputImage>
-::DemonsPreprocessor()
+::DemonsPreprocessor() :
+  m_NumberOfHistogramLevels(256),
+  m_NumberOfMatchPoints(1),
+  m_FixedImageMinimum(NumericTraits<InputPixelType>::NonpositiveMin() ),
+  m_MovingImageMinimum(NumericTraits<InputPixelType>::NonpositiveMin() ),
+  m_FixedBinaryVolume("none"),
+  m_MovingBinaryVolume("none"),
+  m_Lower(NumericTraits<PixelType>::NonpositiveMin() ),
+  m_Upper(NumericTraits<PixelType>::max() ),
+  m_DefaultPixelValue(NumericTraits<PixelType>::One),
+  m_OutDebug(false),
+  m_UseHistogramMatching(0)
 {
-  m_UseHistogramMatching = 0;
-  m_NumberOfHistogramLevels = 256;
-  m_NumberOfMatchPoints = 1;
-
-  m_FixedImageMinimum =  NumericTraits<InputPixelType>::NonpositiveMin();
-  m_MovingImageMinimum = NumericTraits<InputPixelType>::NonpositiveMin();
-
-  m_FixedBinaryVolume = "none";
-  m_MovingBinaryVolume = "none";
-  //    m_Seed =  NumericTraits<IndexType>::Zero;
   for( unsigned i = 0; i < TInputImage::ImageDimension; ++i )
     {
     m_Seed[i] = 0;
     m_MedianFilterSize[i] = 0;
     }
-  m_Lower = NumericTraits<PixelType>::NonpositiveMin();
-  m_Upper = NumericTraits<PixelType>::max();
-
-  m_DefaultPixelValue = NumericTraits<PixelType>::One;
   m_Radius.Fill(1);
-  m_OutDebug = false;
 }
 
 template <typename TInputImage, typename TOutputImage>
@@ -111,7 +107,6 @@ DemonsPreprocessor<TInputImage, TOutputImage>
     histogramfilter->SetNumberOfMatchPoints(m_NumberOfMatchPoints);
     histogramfilter->ThresholdAtMeanIntensityOn();
     histogramfilter->Update();
-    //      m_OutputFixedImage  = histogramfilter->GetOutput();
     m_OutputMovingImage  = histogramfilter->GetOutput();
     // +DANGER: ALIASING:  m_OutputMovingImage EQ m_UnNormalizedMovingImage by
     // design.
@@ -119,9 +114,6 @@ DemonsPreprocessor<TInputImage, TOutputImage>
     // m_OutputMovingImage =
     // itkUtil::CopyImage<TOutputImage>(m_UnNormalizedMovingImage);
     }
-
-  //  m_OutputMovingImage =
-  // itkUtil::CopyImage<TOutputImage>(m_UnNormalizedMovingImage);
 
   m_OutputFixedImage = itkUtil::CopyImage<TOutputImage>(
       m_UnNormalizedFixedImage);

BRAINSCommonLib/itkOtsuHistogramMatchingImageFilter.hxx
@@ -29,30 +29,25 @@ namespace itk
   */
 template <class TInputImage, class TOutputImage, class THistogramMeasurement>
 OtsuHistogramMatchingImageFilter<TInputImage, TOutputImage, THistogramMeasurement>
-::OtsuHistogramMatchingImageFilter()
+::OtsuHistogramMatchingImageFilter() :
+  m_NumberOfHistogramLevels(256),
+  m_NumberOfMatchPoints(1),
+  m_SourceMinValue(0),
+  m_ReferenceMinValue(0),
+  m_SourceHistogram(HistogramType::New() ),
+  m_ReferenceHistogram(HistogramType::New() ),
+  m_OutputHistogram(HistogramType::New() ),
+  m_LowerGradient(0.0),
+  m_UpperGradient(0.0),
+  m_SourceMask(NULL),
+  m_ReferenceMask(NULL)
 {
   this->SetNumberOfRequiredInputs(2);
-
-  m_NumberOfHistogramLevels = 256;
-  m_NumberOfMatchPoints = 1;
 
   m_QuantileTable.set_size(3, m_NumberOfMatchPoints + 2);
   m_QuantileTable.fill(0);
   m_Gradients.set_size(m_NumberOfMatchPoints + 1);
   m_Gradients.fill(0);
-
-  m_LowerGradient = 0.0;
-  m_UpperGradient = 0.0;
-
-  // Create histograms.
-  m_SourceHistogram = HistogramType::New();
-  m_ReferenceHistogram = HistogramType::New();
-  m_OutputHistogram = HistogramType::New();
-
-  m_SourceMinValue = 0;
-  m_ReferenceMinValue = 0;
-  m_SourceMask = NULL;
-  m_ReferenceMask = NULL;
 }
 
 /*
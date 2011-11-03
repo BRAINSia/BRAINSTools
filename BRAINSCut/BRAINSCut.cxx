06bbd69e1b6d55d72694edf887253f2fe3151088
Regina Kim <eunyoung-kim@uiowa.edu>
2011-11-03 09:59:16 -0500

    ENH: Temp. Sol.of Warp File (ITK4) Thanks,Kent!

--- BRAINSCut/BRAINSCut.cxx
+++ BRAINSCut/BRAINSCut.cxx
@@ -18,8 +18,12 @@ int main(int argc, char * *argv)
 {
   PARSE_ARGS;
 
-  /** Test of Class
+  /* Solution from Kent
+   * ITK4 resigration initilization
    */
+  // Call register default transforms
+  itk::TransformFactoryBase::RegisterDefaultTransforms();
+
   if( generateProbability )
     {
     BRAINSCutGenerateProbability testBRAINSCutClass( netConfiguration );
@@ -62,77 +66,5 @@ int main(int argc, char * *argv)
       }
     }
 
-/* DELETE THIS
-  BRAINSUtils::SetThreadCount(numberOfThreads);
-
-  // Apparently when you register one transform, you need to register all your
-  // transforms.
-  itk::AddExtraTransformRegister();
-
-  int status = 0;
-  if( netConfiguration == "" )
-    {
-    std::cerr << "No XML Configuration file given" << std::endl;
-    std::cerr.flush();
-    }
-  if( generateProbability )
-    {
-    extern int GenerateProbability(const std::string & xmlFile,
-                                   int verbose,
-                                   bool validate);
-    status = GenerateProbability(netConfiguration,
-                                 verbose,
-                                 validate);
-    if( verbose > 9 )
-      {
-      std::cout << " Status Returned from GenerateProbability is "
-                << status
-                << std::endl;
-      }
-    }
-  if( status == 0 && createVectors )
-    {
-    extern int CreateVectors(const std::string & xmlFile,
-                             bool histogramEqualization,
-                             int verbose);
-    status = CreateVectors(netConfiguration,
-                           histogramEqualization,
-                           verbose);
-    if( verbose > 9 )
-      {
-      std::cout << " Status Returned from CreateVectors is "
-                << status
-                << std::endl;
-      }
-    }
-  if( status == 0 && trainModel )
-    {
-    extern int TrainModel(const std::string & xmlFile,
-                          int verbose,
-                          int start_iteration
-                          );
-    status = TrainModel(netConfiguration,
-                        verbose,
-                        trainModelStartIndex);
-    if( verbose > 9 )
-      {
-      std::cout << " Status Returned from CreateVectors is "
-                << status
-                << std::endl;
-      }
-    }
-  if( status == 0 && applyModel )
-    {
-    extern int ApplyModel(const std::string & xmlFile,
-                          int verbose,
-                          bool histogramEqualization,
-                          bool multiStructureThreshold);
-    status = ApplyModel(netConfiguration,
-                        verbose,
-                        histogramEqualization,
-                        multiStructureThreshold
-                        );
-    }
-    */
   return EXIT_SUCCESS;
 }

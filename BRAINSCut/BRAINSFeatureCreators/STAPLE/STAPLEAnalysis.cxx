63a06ebcf8210a727686ac0832ca33ba781038c3
Regina Kim <eunyoung-kim@uiowa.edu>
2011-12-14 13:10:23 -0600

    ADD:STAPLE

---
+++ BRAINSCut/BRAINSFeatureCreators/STAPLE/STAPLEAnalysis.cxx
@@ -0,0 +1,109 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkMultiLabelSTAPLEImageFilter.h"
+// #include "itkSTAPLEImageFilter.h"
+#include "itkTimeProbe.h"
+
+#include <string>
+#include <fstream.h>
+
+#include "STAPLEAnalysisCLP.h"
+template <unsigned int ImageDimension>
+int STAPLE(unsigned int argc, char *argv[] )
+{
+  PARSE_ARGS;
+
+  itk::TimeProbe timer;
+  timer.Start();
+
+  typedef float RealType;
+
+  typedef itk::Image<unsigned int, ImageDimension> LabelImageType;
+
+  typedef itk::MultiLabelSTAPLEImageFilter<LabelImageType, LabelImageType> FilterType;
+
+  typename FilterType::Pointer filter = FilterType::New();
+
+  typedef std::vector<std::string>                  StringVectorType;
+  typedef typename StringVectorType::const_iterator StringVectorIteratorType;
+
+  StringVectorIteratorType currentLabel = inputLabelVolume.begin();
+  for( unsigned int i = 0;
+       i < inputLabelVolume.size();
+       i++, ++currentLabel )
+    {
+    typedef itk::ImageFileReader<LabelImageType> ReaderType;
+    typename ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName( *currentLabel );
+    reader->Update();
+
+    filter->SetInput( i, reader->GetOutput() );
+    }
+  filter->Update();
+  for( unsigned int i = 0; i < inputLabelVolume.size(); i++ )
+    {
+//    std::cout << "Confusion matrix assessment: " << argv[i] << std::endl;
+//    std::cout << filter->GetConfusionMatrix( i-3 ) << std::endl << std::endl;
+
+    typename FilterType::ConfusionMatrixType conf =
+      filter->GetConfusionMatrix( i );
+    for( unsigned int j = 0; j < conf.rows(); j++ )
+      {
+      for( unsigned int k = 0; k < conf.cols(); k++ )
+        {
+        std::cout << conf( j, k );
+        if( j == conf.rows() - 1 && k == conf.cols() - 1 )
+          {
+          std::cout << std::endl;
+          }
+        else
+          {
+          std::cout << ",";
+          }
+        }
+      }
+    }
+
+  typename FilterType::PriorProbabilitiesType priors =
+    filter->GetPriorProbabilities();
+
+//  std::cout << "Prior probabilities" << std::endl;
+//  for ( unsigned int i = 0; i < priors.GetSize(); ++i )
+//    {
+//    std::cout << "\t" << i << ": " << priors[i] << "\n";
+//    }
+
+  typedef itk::ImageFileWriter<LabelImageType> WriterType;
+  typename WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( outputVolume );
+  writer->SetInput( filter->GetOutput() );
+  writer->Update();
+
+  timer.Stop();
+
+  return EXIT_SUCCESS;
+}
+
+int main( int argc, char *argv[] )
+{
+  PARSE_ARGS;
+  if( argc < 3 )
+    {
+    std::cout << "Usage: " << argv[0] << " imageDimension outputImage "
+              << "segmentationImage1 ... segmentationImageN" << std::endl;
+    exit( 0 );
+    }
+
+  switch( inputDimension )
+    {
+    case 2:
+      STAPLE<2>( argc, argv);
+      break;
+    case 3:
+      STAPLE<3>( argc, argv);
+      break;
+    default:
+      std::cerr << "Unsupported dimension" << std::endl;
+      exit( EXIT_FAILURE );
+    }
+}

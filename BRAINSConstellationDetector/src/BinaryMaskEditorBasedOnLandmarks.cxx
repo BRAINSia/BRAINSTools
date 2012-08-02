3eb2d789b34d0b7510c04554ad059db3af874f33
Regina Kim <eunyoung.regina.kim@gmail.com>
2012-08-02 12:17:17 -0500

    Mask editor for DTI
    Index bug fix
    
    Fix minor bug in the parameter number checking

--- BRAINSConstellationDetector/src/BinaryMaskEditorBasedOnLandmarks.cxx
+++ BRAINSConstellationDetector/src/BinaryMaskEditorBasedOnLandmarks.cxx
@@ -2,10 +2,15 @@
 // Edit binary mask based on given landmark and direction.
 //
 #include "Slicer3LandmarkIO.h"
+#include "itkImageFileReader.h"
+#include "itkImageDuplicator.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageFileWriter.h"
+#include "vcl_algorithm.h"
 
 #include "BinaryMaskEditorBasedOnLandmarksCLP.h"
 
-class PlaneLandmarksType
+class ThreeLandmarksForPlane
 {
   // The definition depends on Slicer3LandmarkIO.
   // Point type is simply itk::Pointe<double, 3>
@@ -13,28 +18,63 @@ public:
   PointType A;
   PointType B;
   PointType C;
-private:
-  PointType normal;
+
+  typedef itk::Point<double, 3> VectorType;
 
   void SetNormal()
   {
     // Determine AB and AC vector components
-    PointType AB;
+    VectorType AB;
 
     AB[0] = B[0] - A[0];
     AB[1] = B[1] - A[1];
     AB[2] = B[2] - A[2];
+    std::cout << "AB::: ["
+              << AB[0] << ", "
+              << AB[1] << ", "
+              << AB[2] << " ]"
+              << std::endl;
 
-    PointType AC;
+    VectorType AC;
     AC[0] = C[0] - A[0];
     AC[1] = C[1] - A[1];
     AC[2] = C[2] - A[2];
+    std::cout << "AC::: ["
+              << AC[0] << ", "
+              << AC[1] << ", "
+              << AC[2] << " ]"
+              << std::endl;
 
     // Find cross product components
 
     normal[0] = ( AB[1] * AC[2] ) - ( AC[1] * AB[2] );
     normal[1] = -1 * ( ( AB[0] * AC[2] ) - ( AC[0] * AB[2] ) );
     normal[2] = ( AB[0] * AC[1] ) - ( AC[0] * AB[1] );
+
+    double magnitude = vcl_sqrt( normal[0] * normal[0]
+                                 + normal[1] * normal[1]
+                                 + normal[2] * normal[2] );
+
+    std::cout << "Normal::: Before Norm["
+              << normal[0] << ", "
+              << normal[1] << ", "
+              << normal[2] << " ]"
+              << std::endl;
+    normal[0] = normal[0] / magnitude;
+    normal[1] = normal[1] / magnitude;
+    normal[2] = normal[2] / magnitude;
+
+    if( normal[0] < 0.0F ) // Fix X-axis to the positive for consistency
+      {
+      normal[0] = -normal[0];
+      normal[1] = -normal[1];
+      normal[2] = -normal[2];
+      }
+    std::cout << "Normal:::  After Norm["
+              << normal[0] << ", "
+              << normal[1] << ", "
+              << normal[2] << " ]"
+              << std::endl;
   }
 
   double GetRelativeLocationToPlane( PointType x )
@@ -46,7 +86,151 @@ private:
 
     return answer;
   }
+
+  VectorType GetNormal()
+  {
+    return normal;
+  }
+
+private:
+  VectorType normal;
 };
+
+// ------------------------------------------------------------------------ //
+template <class TImageType>
+void
+CutBinaryVolumeByPlaneWithDirection( typename TImageType::Pointer * _imageVolume,
+                                     ThreeLandmarksForPlane * currentPlane,
+                                     const std::string direction )
+{
+  typedef itk::ImageRegionIterator<TImageType> ImageRegionIteratorType;
+  ImageRegionIteratorType it(  *_imageVolume,
+                               (*_imageVolume)->GetRequestedRegion() );
+  std::cout << __LINE__ << "::" << __FILE__ << std::endl;
+  for( it.GoToBegin(); !it.IsAtEnd(); ++it )
+    {
+    PointType currentPhysicalLocation;
+    (*_imageVolume)->TransformIndexToPhysicalPoint( it.GetIndex(), currentPhysicalLocation );
+
+    if( direction == "true" &&
+        (*currentPlane).GetRelativeLocationToPlane( currentPhysicalLocation ) > 0.0F )
+      {
+      it.Set( 0.0F );
+      }
+    else if( direction == "false" &&
+             (*currentPlane).GetRelativeLocationToPlane( currentPhysicalLocation ) < 0.0F )
+      {
+      it.Set( 0.0F );
+      }
+    }
+}
+
+// ------------------------------------------------------------------------ //
+template <class TImageType>
+void
+CutBinaryVolumeByPointWithDirection( typename TImageType::Pointer * _imageVolume,
+                                     const PointType _landmark,
+                                     const std::string _direction )
+{
+  // set directional constant for convenient programming
+  enum DIRECTION
+    {
+    ANTERIOR, POSTERIOR, LEFT, RIGHT, SUPERIOR, INFERIOR
+    } myDirection;
+
+  if( _direction == "ANTERIOR" )
+    {
+    myDirection = ANTERIOR;
+    }
+  else if( _direction == "POSTERIOR" )
+    {
+    myDirection = POSTERIOR;
+    }
+  else if( _direction == "LEFT" )
+    {
+    myDirection = LEFT;
+    }
+  else if( _direction == "RIGHT" )
+    {
+    myDirection = RIGHT;
+    }
+  else if( _direction == "SUPERIOR" )
+    {
+    myDirection = SUPERIOR;
+    }
+  else if( _direction == "INFERIOR" )
+    {
+    myDirection = INFERIOR;
+    }
+
+  typedef itk::ImageRegionIterator<TImageType> ImageRegionIteratorType;
+  ImageRegionIteratorType it(  *_imageVolume,
+                               (*_imageVolume)->GetRequestedRegion() );
+  std::cout << __LINE__ << "::" << __FILE__ << std::endl;
+  for( it.GoToBegin(); !it.IsAtEnd(); ++it )
+    {
+    PointType currentPhysicalLocation;
+    (*_imageVolume)->TransformIndexToPhysicalPoint( it.GetIndex(), currentPhysicalLocation );
+
+    switch( myDirection )
+      {
+      case RIGHT:
+        {
+        if( currentPhysicalLocation[0] > _landmark[0] )
+          {
+          it.Set( 0.0F );
+          }
+        ;                                                                     // RIGHT
+        }
+        break;
+      case LEFT:
+        {
+        if( currentPhysicalLocation[0] < _landmark[0] )
+          {
+          it.Set( 0.0F );
+          }
+        ;                                                                     // LEFT
+        }
+        break;
+      case ANTERIOR:
+        {
+        if( currentPhysicalLocation[1] > _landmark[1] )
+          {
+          it.Set( 0.0F );
+          }
+        ;                                                                     // ANTERIOR
+        }
+        break;
+      case POSTERIOR:
+        {
+        if( currentPhysicalLocation[1] < _landmark[1] )
+          {
+          it.Set( 0.0F );
+          }
+        ;                                                                     // POSTERIOR
+        }
+        break;
+      case INFERIOR:
+        {
+        if( currentPhysicalLocation[2] > _landmark[2] )
+          {
+          it.Set( 0.0F );
+          }
+        ;                                                                     // INFERIOR
+        }
+        break;
+      case SUPERIOR:
+        {
+        if( currentPhysicalLocation[2] < _landmark[2] )
+          {
+          it.Set( 0.0F );
+          }
+        ;                                                                     // SUPERIOR
+        }
+        break;
+      }
+    }
+}
 
 int main( int argc, char * argv[] )
 {
@@ -54,12 +238,12 @@ int main( int argc, char * argv[] )
 
   // check input
   if( inputBinaryVolume.empty() ||
-      inputLandmarksFilename.empty() ||
-      inputLandmarkNames.empty() )
+      inputLandmarksFilename.empty()
+      )
     {
-    std::cout << "Input Landmarks Filename ( inputLandmarkFilename ) ,"
-              << "input binary volume ( inputBinaryVolume ) and "
-              << "input landmark name (inputLandmarkNames) are necessary."
+    std::cout << "Input Landmarks Filename ( inputLandmarkFilename ) and"
+              << "input binary volume ( inputBinaryVolume ) "
+              << " are necessary."
               << std::endl;
     exit(EXIT_FAILURE);
     }
@@ -81,6 +265,7 @@ int main( int argc, char * argv[] )
 
   ImageReaderType::Pointer imageReader = ImageReaderType::New();
   imageReader->SetFileName( inputBinaryVolume );
+  imageReader->Update();
 
   ImageType::Pointer inputVolume = imageReader->GetOutput();
 
@@ -90,8 +275,8 @@ int main( int argc, char * argv[] )
   LandmarksMapType landmarksSet = ReadSlicer3toITKLmk( inputLandmarksFilename );
 
   // duplicate image
-  typdef itk::ImageDuplicator<ImageType> ImageDuplicatorType;
-  ImageDuplicatorType::Pointer           duplicator = ImageDuplicatorType::New();
+  typedef itk::ImageDuplicator<ImageType> ImageDuplicatorType;
+  ImageDuplicatorType::Pointer duplicator = ImageDuplicatorType::New();
   duplicator->SetInputImage( inputVolume );
   duplicator->Update();
 
@@ -100,8 +285,8 @@ int main( int argc, char * argv[] )
   // cut by landmarks
   typedef std::vector<std::string>::const_iterator stringVectorIteratorType;
   for( stringVectorIteratorType ldmkIt = inputLandmarkNames.begin(),
-       stringVectorIteratorType dircIt = setCutDirectionForLandmark.begin();
-       ldmkIt < inputLandmarkNames.end(),
+       dircIt = setCutDirectionForLandmark.begin();
+       ldmkIt < inputLandmarkNames.end();
        ldmkIt++, dircIt++ )
     {
     if( landmarksSet.find( *ldmkIt ) == landmarksSet.end() )
@@ -110,50 +295,98 @@ int main( int argc, char * argv[] )
       std::exit( EXIT_FAILURE );
       }
 
-    // TODO: implement CutBinaryVolumeByPointWithDirection
-    outputVolume =
-      CutBinaryVolumeByPointWithDirection( outputVolume, *ldmkIt, *dircIt );
+    PointType currentLdmk = landmarksSet.find( *ldmkIt )->second;
+    std::cout << "currentLdmk:: " << *ldmkIt << "::"
+              << currentLdmk[0] << ", "
+              << currentLdmk[1] << ", "
+              << currentLdmk[2] << std::endl;
+
+    CutBinaryVolumeByPointWithDirection<ImageType>( &outputVolume, currentLdmk, *dircIt );
     }
 
   // cut by plane defined by three landmarks
 
-  typedef std::vector<std::string>           PlaneLandmarksType;
-  typedef PlaneLandmarksType::const_iterator ThreeLandmarkIteratorType;
+  // string vector to read in a vector of three elements.
+  // ex) (LE, RE, PC) --> plane for left and right eyes with posterior commissure.
+  typedef std::vector<std::string> LandMarkForPlaneType;
 
-  typedef std::vector<PlaneLandmarksType>      PlaneLandmarkSetType;
+  // a set of plane description
+  // ex) 1: (LE, RE, PC)
+  //     2: (AC, PC, LE)
+  //     3: ....
+  typedef std::vector<ThreeLandmarksForPlane>  PlaneLandmarkSetType;
   typedef PlaneLandmarkSetType::const_iterator PlaneLandmarkSetIteratorType;
-  for( PlaneLandmarkSetIteratorType planeIt = inputLandmarkNamesForObliquePlane.begin();
-       planeIt < inputLandmarkNamesForObliquePlane.end();
-       planeIt++ )
-    {
-    PlaneLandmarksType currentPlane;
 
-    ThreeLandmarkIteratorType tlmIt = ( *planeIt )->begin();
+  PlaneLandmarkSetType myLandmarkSetForPlanes;
+
+  if( inputLandmarkNamesForObliquePlane.size() % 3 != 0 )
+    {
+    std::cerr << "ERROR: Landmarks set for plane has to given as a multiple of three: "
+              << inputLandmarkNamesForObliquePlane.size() << std::endl;
+    std::exit( EXIT_FAILURE );
+    }
+  stringVectorIteratorType planeDir = setCutDirectionForObliquePlane.begin();
+
+  if( inputLandmarkNamesForObliquePlane.size() != 0 )
+    {
+    if( inputLandmarkNamesForObliquePlane.size() !=
+        3 * setCutDirectionForObliquePlane.size() )
+      {
+      std::cout << "Directional information for plane has to be match "
+                << "to the number of plane descriptions.("
+                << inputLandmarkNamesForObliquePlane.size() << "!="
+                << setCutDirectionForObliquePlane.size() << ")"
+                << std::endl;
+      return EXIT_FAILURE;
+      }
+    }
+  for( LandMarkForPlaneType::const_iterator  inputLdmrIt = inputLandmarkNamesForObliquePlane.begin();
+       inputLdmrIt < inputLandmarkNamesForObliquePlane.end();
+       inputLdmrIt++ )
+    {
+    ThreeLandmarksForPlane currentPlane;
     for( unsigned int it = 0; it < 3; it++  ) // make sure we have only three ldmr per plane
       {
-      if( landmarksSet.find( *tlmIt ) ==  landmarksSet.end() )
+      if( landmarksSet.find( *inputLdmrIt ) ==  landmarksSet.end() )
         {
-        std::cerr << "ERROR: Landmark not found: " << *tlmIt << std::endl;
+        std::cerr << "ERROR: Landmark not found: " << *inputLdmrIt << std::endl;
         std::exit( EXIT_FAILURE );
+        }
+      PointType currentLandmark = landmarksSet.find( *inputLdmrIt )->second;
+      std::cout << "currentLandmark:: " << *inputLdmrIt << "::"
+                << currentLandmark[0] << ", "
+                << currentLandmark[1] << ", "
+                << currentLandmark[2] << std::endl;
+      if( it == 0 )
+        {
+        currentPlane.A = currentLandmark;
         }
       if( it == 1 )
         {
-        currentPlane.A = *tlmIt;
+        currentPlane.B = currentLandmark;
         }
       if( it == 2 )
         {
-        currentPlane.B = *tlmIt;
-        }
-      if( it == 3 )
-        {
-        currentPlane.C = *tlmIt;
+        currentPlane.C = currentLandmark;
         }
 
-      tlmIt++;
+      inputLdmrIt++;
       }
     currentPlane.SetNormal();
+    myLandmarkSetForPlanes.push_back(currentPlane);
 
-    outputVolume =
-      CutBinaryVolumeByPlaneWithDirection( outputVolume, *currentPlane, *direction );
+    CutBinaryVolumeByPlaneWithDirection<ImageType>( &outputVolume, &currentPlane, *planeDir );
+    ++planeDir;
     }
+
+  // write output volume
+
+  typedef itk::ImageFileWriter<ImageType> OutputImageWriterType;
+  OutputImageWriterType::Pointer writer = OutputImageWriterType::New();
+
+  writer->SetInput( outputVolume );
+  writer->SetFileName( outputBinaryVolume );
+  writer->Update();
+
+  return EXIT_SUCCESS;
 }

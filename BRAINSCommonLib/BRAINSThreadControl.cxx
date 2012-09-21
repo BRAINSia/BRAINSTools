1cb772dcbd0a44092f22c382e82077c1d4986134
Hans Johnson <hans-johnson@uiowa.edu>
2012-09-21 12:27:16 -0500

    BUG:  Number of threads was not being properly respected
    Whatever then enduser requests for number of threads
    on the command line should be respected, no matter what
    the environmental variables are set to.

--- BRAINSCommonLib/BRAINSThreadControl.cxx
+++ BRAINSCommonLib/BRAINSThreadControl.cxx
@@ -10,23 +10,26 @@ StackPushITKDefaultNumberOfThreads::StackPushITKDefaultNumberOfThreads(const int
 
   if( desiredCount > 0 )  // NOTE: Default is -1, which then uses the ITK default.
     {
+    // Respect the users request irregardless of what environmental variables are set to.
     threadCount = desiredCount;
     }
   else
-    {                                          //
+    {                                          // If user set desiredCount <= 0, then use evnironmentanl or internal
+                                               // ITKv4 values.
     threadCount = this->m_originalThreadValue; // This is the old default.
-    }
-    { // Process the NSLOTS environmental varialble set by the SGE batch processing system
-    int         NSLOTSThreadCount(-1);
-    std::string numThreads;
-    if( itksys::SystemTools::GetEnv("NSLOTS", numThreads) )
-      {
-      std::istringstream s(numThreads, std::istringstream::in);
-      s >> NSLOTSThreadCount;
-      }
-    if( NSLOTSThreadCount > threadCount )
-      {
-      threadCount = NSLOTSThreadCount;
+      {                                        // Process the NSLOTS environmental varialble set by the SGE batch
+                                               // processing system
+      int         NSLOTSThreadCount(-1);
+      std::string numThreads;
+      if( itksys::SystemTools::GetEnv("NSLOTS", numThreads) )
+        {
+        std::istringstream s(numThreads, std::istringstream::in);
+        s >> NSLOTSThreadCount;
+        }
+      if( NSLOTSThreadCount > threadCount )
+        {
+        threadCount = NSLOTSThreadCount;
+        }
       }
     }
   if( threadCount > 0 )

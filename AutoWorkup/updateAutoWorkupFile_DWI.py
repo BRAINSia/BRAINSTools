41ea982dbc6e256e178b5bf860771b697c699cde
Jessica Forbes <jessica-forbes@uiowa.edu>
2012-11-02 10:25:47 -0500

    ENH: Mostly finished with the script that updates the autoworkup csv file with new filepaths for a specified modality.

--- AutoWorkup/updateAutoWorkupFile_DWI.py
+++ AutoWorkup/updateAutoWorkupFile_DWI.py
@@ -2,14 +2,9 @@ import argparse
 import csv
 import os
 import textwrap
+import sqlite3 as lite
 
 class UpdateAutoWorkup():
-
-    def _generateNewPathName(self):
-        dirname = os.path.dirname(inputArguments.autoWorkupFile)
-        basename = os.path.basename(inputArguments.autoWorkupFile)
-        newPath = os.path.join(dirname, "edited_{0}".format(basename))
-        return newPath
 
     def updateAutoWorkup(self):
         newPath = self._generateNewPathName()
@@ -24,97 +19,116 @@ class UpdateAutoWorkup():
                 project = row[0]
                 subject = row[1]
                 session = row[2]
+                newImagesList = NewImageDict.getNewImagesList(project, subject, session)
                 scanDict = eval(row[3])
-                newScanDict = dict()
-                if project =
-                for scan in scanDict.keys():
-                    filepaths = scanDict[scan]
-                    for path in filepaths:
-                        if path in blackListKeys:
-                            newPath = blackListDict[path]
-                            if newPath == '':
-                                continue
-                            if scan not in newScanDict.keys():
-                                newScanDict[scan] = [newPath]
-                            else:
-                                newScanDict[scan].append(newPath)
-                        else:
-                            if scan not in newScanDict.keys():
-                                newScanDict[scan] = [path]
-                            else:
-                                newScanDict[scan].append(path)
-                if newScanDict != {}:
-                    project = row[0]
-                    subject = row[1]
-                    session = row[2]
-                    line = (project, subject, session, newScanDict)
-                    newFile.writerow(line)
+                if newImagesList == []:
+                    pass
+                else:
+                    if inputArguments.modality not in scanDict.keys():
+                        scanDict[inputArguments.modality] = newImagesList
+                    else:
+                        for filepath in newImagesList:
+                            if filepath not in scanDict[inputArguments.modality]:
+                                scanDict[inputArguments.modality].append(filepath)
+                line = (project, subject, session, scanDict)
+                newFile.writerow(line)
+
+    def _generateNewPathName(self):
+        dirname = os.path.dirname(inputArguments.autoWorkupFile)
+        basename = os.path.basename(inputArguments.autoWorkupFile)
+        newPath = os.path.join(dirname, "{}_{}".format(inputArguments.modality, basename))
+        return newPath
 
 class MakeNewImageDict():
 
     def __init__(self):
         self.newImageDict = dict()
-        self._populateNewImageDict()
+        self.commandList = list()
+        self.dbName = 'NewImages.db'
+        self.dbTableName = 'NewImages'
+        self.newImagesFilepath = 'DWI_Images.list'
+        self._makeNewImagesFile()
+        self._makeDB()
+        self._createCommandList()
+        self._fillDB()
 
-    def _populateNewImageDict(self):
-        handle = csv.reader(open(inputArguments.blackList, 'rb'), delimiter=',', quotechar='\"')
+    def _makeNewImagesFile(self):
+        command = 'find %s -name "*_DWI_CONCAT_QCed.nrrd" |awk -F/ \'{print "%s," $5 "," $6 "," $7 "," $0}\' |tee %s'%(
+            inputArguments.inputDir, inputArguments.modality, self.newImagesFilepath)
+        os.system(command)
+
+    def _createCommandList(self):
+        handle = csv.reader(open(self.newImagesFilepath, 'rb'), delimiter=',', quotechar='\"')
         for row in handle:
-            if len(row) == 5:
-                #imageInfo = {'modality': row[0],'project': row[1], 'subject': row[2],
-                #             'session': row[3], 'imagefile': row[4]}
-                modality = row[0]
-                project = row[1]
-                subject = row[2]
-                session = row[3]
-                imagefile = row[4]
-                self._addModality(modality)
-                self._addProject(modality, project)
-                self._addSubject(modality, project, subject)
-                self._addSession(modality, project, subject, session)
-                self._addImageFile(modality, project, subject, session, imagefile)
-            else:
-                print("WARNING: WRONG # of columns in csv (should be 5): {0}".format(row))
+            if handle.line_num > 1:
+                if len(row) == 5:
+                    imageInfo = {'modality': row[0],'project': row[1], 'subject': row[2],
+                                 'session': row[3], 'filepath': row[4]}
+                    sqlCommand = self._makeSQLiteCommand(imageInfo)
+                    self._appendCommand(sqlCommand)
+                else:
+                    print("WARNING: Wrong number of columns in csv file (should be 5): {0}".format(row))
 
-    def _addModality(self, modality):
-        if modality not in self.newImageDict.keys():
-            self.newImageDict[modality] = dict()
+    def _makeDB(self):
+        if os.path.exists(self.dbName):
+            os.remove(self.dbName)
+        dbColTypes =  "modality TEXT, project TEXT, subject TEXT, session TEXT, filepath TEXT"
+        con = lite.connect(self.dbName)
+        dbCur = con.cursor()
+        dbCur.execute("CREATE TABLE {dbTableName}({dbColTypes});".format(dbColTypes=dbColTypes,dbTableName=self.dbTableName))
+        dbCur.close()
 
-    def _addProject(self, modality, project):
-        if project not in self.newImageDict[modality].keys():
-            self.newImageDict[modality][project] = dict()
+    def _fillDB(self):
+        con = lite.connect(self.dbName)
+        dbCur = con.cursor()
+        for command in self.commandList:
+            dbCur.execute(command)
+            con.commit()
+        dbCur.close()
 
-    def _addSubject(self, modality, project, subject):
-        if subject not in self.newImageDict[modality][project].keys():
-            self.newImageDict[modality][project][subject] = dict()
+    def _makeSQLiteCommand(self, imageDict):
+        keys = imageDict.keys()
+        vals = imageDict.values()
+        col_names = ",".join(keys)
+        values = ', '.join(map(lambda x: "'" + x + "'", vals))
+        sqlCommand = "INSERT INTO {dbTableName} ({col_names}) VALUES ({values});".format(dbTableName=self.dbTableName,
+                                                                                         col_names=col_names, values=values)
+        return sqlCommand
 
-    def _addSession(self, modality, project, subject, session):
-        if session not in self.newImageDict[modality][project][subject].keys():
-            self.newImageDict[modality][project][subject][session] = list()
+    def _appendCommand(self, val):
+        self.commandList.append(val)
 
-    def _addImageFile(self, modality, project, subject, session, imagefile):
-        if imagefile not in self.newImageDict[modality][project][subject][session]:
-            self.newImageDict[modality][project][subject][session].append(imagefile)
+    def getNewImagesList(self, project, subject, session):
+        sqlQuery = self._makeDBquery( project, subject, session)
+        dbInfo = self._getInfoFromDB(sqlQuery)
+        newImages = list()
+        for item in dbInfo:
+            newImages.append(str(item[0]))
+        return newImages
+
+    def _getInfoFromDB(self, sqlQuery):
+        con = lite.connect(self.dbName)
+        dbCur = con.cursor()
+        dbCur.execute(sqlQuery)
+        dbInfo = dbCur.fetchall()
+        dbCur.close()
+        return dbInfo
+
+    def _makeDBquery(self, project, subject, session):
+        return "SELECT filepath FROM {dbTableName} WHERE modality='{modality}' AND project='{project}' AND subject='{subject}' AND session='{session}';".format(
+            dbTableName=self.dbTableName, modality=inputArguments.modality, project=project, subject=subject, session=session)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description=textwrap.dedent("""
-This program is used to update the Auto Workup input csv file with the black list. \n
-The blacklist needs to be a comma separated file.
-    Example line:
-    file/path/you/desire/to/change, new/file/path \n
-If a file path is to deleted from the auto workup file, leave the
-new/file/path blank in the blacklist file, but keep the comma:
-    Example line:
-    filepath/you/desire/to/change,
+This program is used to update the Auto Workup input csv file with new filepaths. \n
 
-Common Usage: \n
-$ python updateAutoWorkupFile.py -a autoworkupfile -b blacklistfile
-
-Example:
-$ python updateAutoWorkupFile.py -a example_autoworkup.csv -b example_blacklist.csv"""))
+Example Usage: \n
+$ python updateAutoWorkupFile_DWI.py -a example_autoworkup.csv -m DWI -d /paulsen/Experiments/20120814_DTIPREP
+"""))
     parser.add_argument('-a', '--autoWorkupFile', action='store', dest='autoWorkupFile', help='')
-    parser.add_argument('-b', '--blackList', action='store', dest='blackList', help='')
     parser.add_argument('-m', '--modality', action='store', dest='modality', help='T1-30, T2-30, or DWI')
+    parser.add_argument('-d', '--inputDir', action='store', dest='inputDir', help='Directory to search through for the new filepaths')
     inputArguments = parser.parse_args()
     Object = UpdateAutoWorkup()
     Object.updateAutoWorkup()

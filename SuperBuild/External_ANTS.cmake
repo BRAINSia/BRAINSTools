3359869d1552f80a43da7a348a84bc0cb7215696
David Welch <david.m.welch@gmail.com>
2012-11-08 11:21:37 -0600

    COMP: Updated ANTs tag to reflect history change in BRAINSia/ANTS

--- SuperBuild/External_ANTS.cmake
+++ SuperBuild/External_ANTS.cmake
@@ -7,7 +7,7 @@ set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
 
 set(proj ANTS)
 set(${proj}_GIT_REPOSITORY "git://github.com/BRAINSia/ANTS.git")
-set(${proj}_GIT_TAG "cc2049fdd777aa04775103a74c84e2b3aacd94aa") # Fixed outdated ANTs API
+set(${proj}_GIT_TAG "3975a50d212401dd1dff334fbaef368c8e0f2208") # Fixed bad ANTs API
 
 set(${proj}_DEPENDENCIES ${ITK_EXTERNAL_NAME} SlicerExecutionModel)
 

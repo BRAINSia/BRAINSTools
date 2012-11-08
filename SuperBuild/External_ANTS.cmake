6820574be2e695f82558cba94644bafc99c4109b
David Welch <david.m.welch@gmail.com>
2012-11-08 11:00:31 -0600

    BUG: Fixed build for ANTs in Superbuild

--- SuperBuild/External_ANTS.cmake
+++ SuperBuild/External_ANTS.cmake
@@ -7,7 +7,7 @@ set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
 
 set(proj ANTS)
 set(${proj}_GIT_REPOSITORY "git://github.com/BRAINSia/ANTS.git")
-set(${proj}_GIT_TAG "ac03f63263ec9b9d7497c2f231f8b958f0a97c1e") #Fix -b flag failures
+set(${proj}_GIT_TAG "cc2049fdd777aa04775103a74c84e2b3aacd94aa") # Fixed outdated ANTs API
 
 set(${proj}_DEPENDENCIES ${ITK_EXTERNAL_NAME} SlicerExecutionModel)
 

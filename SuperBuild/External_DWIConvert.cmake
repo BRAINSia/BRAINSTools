a53655ef95515926ce3b3b08fdd67ae38df27b0c
Kent Williams <norman-k-williams@uiowa.edu>
2012-11-30 09:36:09 -0600

    COMP: Adjust to using ITK's build of DCMTK.

--- SuperBuild/External_DWIConvert.cmake
+++ SuperBuild/External_DWIConvert.cmake
@@ -1,22 +1,21 @@
 
-set(DWIConvert_DEPENDENCIES ITKv4 DCMTK SlicerExecutionModel)
+set(DWIConvert_DEPENDENCIES ITKv4 SlicerExecutionModel)
 
 SlicerMacroCheckExternalProjectDependency(DWIConvert)
 
 set(proj DWIConvert)
 
 set(DWIConvert_REPOSITORY
-  ${git_protocol}://github.com/Chaircrusher/NewDicomToNrrdConverter.git)
+  ${git_protocol}://github.com/BRAINSia/DWIConvert.git)
 
-set(DWIConvert_GIT_TAG 6ddfd659a373cca058b1406443336555b2e401e1 )
-set(DWIConvert_DCMTK_ARGS -DDCMTK_DIR:PATH=${DCMTK_DIR} )
+#set(DWIConvert_GIT_TAG 3df5666d9ba100e15fa424b0c0af4176f26a0bf6)
+set(DWIConvert_GIT_TAG f4a18ade3f84f1be010aa6147119f5004bbd4ff0)
 
 ExternalProject_Add(${proj}
   GIT_REPOSITORY ${DWIConvert_REPOSITORY}
   GIT_TAG ${DWIConvert_GIT_TAG}
   ${slicer_external_update}
   SOURCE_DIR ${proj}
-  BINARY_DIR ${proj}-build
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
   -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
@@ -25,9 +24,8 @@ ExternalProject_Add(${proj}
   -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
   -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
   -DBUILD_SHARED_LIBS:BOOL=ON
-  -DUSE_SYSTEM_DCMTK:BOOL=${Slicer_BUILD_DICOM_SUPPORT}
+  -DUSE_SYSTEM_DCMTK:BOOL=ON
   -DITK_DIR=${ITK_DIR}
-  ${DWIConvert_DCMTK_ARGS}
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""

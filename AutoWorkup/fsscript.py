38d9daf007b287bc889c5c72496644b06213b58d
Dave Welch <david.m.welch@gmail.com>
2012-12-08 01:04:25 -0600

    BUG: Updated/fixed module dependencies BUG: Fixed container touch behavior TODO: Create parser groups?

--- AutoWorkup/fsscript.py
+++ AutoWorkup/fsscript.py
@@ -3,8 +3,6 @@ from nipype.interfaces.freesurfer.base import Info
 
 class ReconContainer(object):
     def __init__(self, *args, **kwds):
-        import os.path.join, os.setenv
-        from subprocess import check_call
         self.shell = '/bin/tsch'
         self.required = kwds['required']
         self._checkFiles()
@@ -13,29 +11,31 @@ class ReconContainer(object):
             setattr(self, key, val)
         self.FS_SCRIPT = os.path.join(self.FREESURFER_HOME, self.FS_SCRIPT)
         self._validateEnv()
+        return super(ReconContainer, self).__init__(*args, **kwds)
 
     def setupEnv(self):
-        os.setenv("FREESURFER_HOME", self.FREESURFER_HOME)
-        sp.check_call(["source" self.FS_SCRIPT], shell=self.shell)
-        os.setenv("SUBJECTS_DIR", self.FS_SUBJECT_DIR)
+        from os import setenv
+        from subprocess import check_call
+        setenv("FREESURFER_HOME", self.FREESURFER_HOME)
+        check_call(["source" self.FS_SCRIPT], shell=self.shell)
+        setenv("SUBJECTS_DIR", self.FS_SUBJECT_DIR)
 
     def _checkFiles(self):
         import os
         for fname in self.required:
             fullname = os.path.join(os.getcwd(), fname)
-            if not os.path.exists(fullname):
-                self._touchFile(fullname)
+            self._touchFile(fullname)
             assert os.path.exists(fullname), "Required file does not exist and could not be created: %s" % fullname
 
     def _touchFile(self, fullname):
-        import subprocess as sp
-        sp.check_call(['touch' fullname])
+        from subprocess import check_call
+        check_call(['touch' fullname])
 
     def _validateEnv(self):
-        import os
-        assert os.path.exist(self.FREESURFER_HOME), "FREESURFER_HOME doesn't exist: %s" % self.FREESURFER_HOME
-        assert os.path.exist(self.FS_SUBJECTS_DIR), "SUBJECTS_DIR doesn't exist: %s" % self.FS_SUBJECTS_DIR
-        assert os.path.exist(self.FS_SCRIPT, "FREESURFER script doesn't exist: %s" % self.FS_SCRIPT)
+        import os.path.exists as exists
+        assert exists(self.FREESURFER_HOME), "FREESURFER_HOME doesn't exist: %s" % self.FREESURFER_HOME
+        assert exists(self.FS_SUBJECTS_DIR), "SUBJECTS_DIR doesn't exist: %s" % self.FS_SUBJECTS_DIR
+        assert exists(self.FS_SCRIPT, "FREESURFER script doesn't exist: %s" % self.FS_SCRIPT)
 
 def autoRecon1(*arg, **kwds):
     """
@@ -45,28 +45,31 @@ def autoRecon1(*arg, **kwds):
 
 def normalizeWM(t1, wm_prob):
     """This function will compute the mean value of wm and rescale the image so that the mean WM=100"""
+    from SimpleITK import BinaryThreshold, Cast, LabelStatisticsImageFilter, sitkFloat32, sitkUInt8
+    WM_MEAN_FINAL = 100.0
     WM_THRESHOLD = 0.66
-    wm_mask = sitk.BinaryThreshold(wm_prob, WM_THRESHOLD)
-    ls = sitk.LabelStatisticsImageFilter()
+    wm_mask = BinaryThreshold(wm_prob, WM_THRESHOLD)
+    ls = LabelStatisticsImageFilter()
     ls.Execute(t1, wm_mask)
     wm_value = 1
     myMeasurementMap = ls.GetMeasurementMap(wm_value)
     MeanWM = myMeasurementMap['Mean']
-    t1_new = sitk.Cast(sitk.Cast(t1, sitk.sitkFloat32) * 100.0 / MeanWM, sitk.sitkUInt8)
+    t1_new = Cast(Cast(t1, sitkFloat32) * WM_MEAN_FINAL / MeanWM, sitkUInt8)
     return t1_new
 
 def baw_Recon1(*arg, **kwds):
-    t1 = sitk.ReadImage(kwds['t1Image'])
-    t2 = sitk.ReadImage(kwds['t2Image'])
-    wm = sitk.ReadImage(kwds['wmProbImage'])
-    brain = sitk.ReadImage(kwds['brainLabel'])
+    import SimpleITK import ReadImage, BinaryThreshold
+    t1 = ReadImage(kwds['t1Image'])
+    t2 = ReadImage(kwds['t2Image'])
+    wm = ReadImage(kwds['wmProbImage'])
+    brain = ReadImage(kwds['brainLabel'])
     t1_new = normalizeWM(t1, wm)
-    clipping = sitk.BinaryThreshold(brain, 1, 1000000) - sitk.BinaryThreshold(brain, 5, 5)
+    clipping = BinaryThreshold(brain, 1, 1000000) - BinaryThreshold(brain, 5, 5)
     clipped = t1_new * clipping
 
 def autoRecon2(*arg, **kwds):
     import os.path.sep as sep
-    import subprocess as sp
+    from subprocess import check_call # import subprocess as sp
     dir1 = 'orig' + sep
     dir2 = 'transforms' + sep
     required = (dir1 + '001.mgz',
@@ -85,10 +88,11 @@ def autoRecon2(*arg, **kwds):
     subj_id = kwds['subjID'] # TODO: Potential security risk - need to sanitize
     with open('autoRecon2.log', 'w') as fID:
         container.setupEnv()
-        sp.check_call(['recon-all', '-autorecon2', '-debug', '-subjid', subj_id], stdout=fID, stderr=sp.STDOUT, shell=container.shell)
+        check_call(['recon-all', '-autorecon2', '-debug', '-subjid', subj_id], stdout=fID, stderr=sp.STDOUT, shell=container.shell)
 
 def autoRecon3(*arg, **kwds):
-    import os.path.sep
+    import os.path.sep as sep
+    from subprocess import check_call
     dir1 = '..' + sep + 'scripts' + sep
     dir2 = 'transforms' + sep
     required = (dir2 + 'talairach.lta',
@@ -131,24 +135,16 @@ def autoRecon3(*arg, **kwds):
     subj_id = kwds['subjID'] # TODO: Potential security risk - need to sanitize
     with open('autoRecon3.log', 'w') as fID:
         container.setupEnv()
-        sp.check_call(['recon-all', '-autorecon3', '-debug', '-subjid', subj_id], stdout=fID, stderr=sp.STDOUT, shell=container.shell)
+        check_call(['recon-all', '-autorecon3', '-debug', '-subjid', subj_id], stdout=fID, stderr=sp.STDOUT, shell=container.shell)
 
 def run(*arg, **kwds):
-    import os
-    os.environ['SUBJECTS_DIR'] = kwds['FS_SUBJECTS_DIR']
-    os.environ['FREESURFER_HOME'] = kwds['FREESURFER_HOME']
-
-    # autoRecon1(*arg, **kwds)
-    baw_Recon1(*arg, **kwds)
+    baw_Recon1(*arg, **kwds) # autoRecon1(*arg, **kwds)
     autoRecon2(*arg, **kwds)
     autoRecon3(*arg, **kwds)
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""
-                                     """
-
-    """DELETE LATER: This is an just example of the commands required to run FreeSurfer recon-all:
+    DELETE LATER: This is an just example of the commands required to run FreeSurfer recon-all:
     /bin/tcsh
     setenv FREESURFER_HOME /ipldev/sharedopt/20110601/MacOSX_10.6/freesurfer
     source ${FREESURFER_HOME}/FreeSurferEnv.csh
@@ -158,19 +154,20 @@ if __name__ == "__main__":
     Link to recon-all i/o table:
     http://surfer.nmr.mgh.harvard.edu/fswiki/ReconAllDevTable
 
-    """
-                                     )
+    """)
+    # TODO: Make parser group "Inputs"
     parser.add_argument('-t1', '--T1image', action='store', dest='t1Image', help='Original T1 image')
     parser.add_argument('-t2', '--T2image', action='store', dest='t2Image', help='Original T2 image')
+    parser.add_argument('-i', '--SubjID', action='store', dest='subjID', help='Subject_Session')
+    parser.add_argument('-b', '--BrainLabel', action='store', dest='brainLabel',
+                        help='The normalized T1 image with the skull removed. Normalized 0-110 where white matter=110.')
+    parser.add_argument('-w', '--WMProb', action='store', dest='wmProbImage', help='')
+    # TODO: Make parser group "Environment"
     parser.add_argument('-h', '--FSHomeDir', action='store', dest='FREESURFER_HOME',
                         default='/ipldev/sharedopt/20110601/MacOSX_10.6/freesurfer',
                         help='Location of FreeSurfer (differs for Mac and Linux environments')
     parser.add_argument('-d', '--FSSubjDir', action='store', dest='FS_SUBJECTS_DIR', help='FreeSurfer subjects directory')
     parser.add_argument('-s', '--FSSource', action='store', dest='FS_SCRIPT',
                         default='FreeSurferEnv.csh', help='')
-    parser.add_argument('-i', '--SubjID', action='store', dest='subjID', help='Subject_Session')
-    parser.add_argument('-b', '--BrainLabel', action='store', dest='brainLabel',
-                        help='The normalized T1 image with the skull removed. Normalized 0-110 where white matter=110.')
-    parser.add_argument('-w', '--WMProb', action='store', dest='wmProbImage', help='')
     kwds = vars(parser.parse_args())
     return runAutoRecon(**kwds)

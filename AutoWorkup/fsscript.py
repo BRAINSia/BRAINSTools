b6ca0a3bc9a53f2fd92d92a9962d98c8c4add9ff
Hans Johnson <hans-johnson@uiowa.edu>
2012-12-10 14:38:33 -0600

    ENH: Fixed logic to add custom fix-up commands for brain mask.

--- AutoWorkup/fsscript.py
+++ AutoWorkup/fsscript.py
@@ -41,9 +41,13 @@ def IsFirstNewerThanSecond(firstFile, secondFile):
         return True
     image_time=os.path.getmtime(firstFile)
     if not os.path.exists(secondFile):
+        print "Returning True because file is missing:  {0}".format(secondFile)
         return True
     reference_time=os.path.getmtime(secondFile)
-    return image_time > reference_time
+    if image_time > reference_time:
+        print "Returning True because {0} is newer than {1}".format(firstFile,secondFile)
+        return True
+    return False
 
 
 def run_mri_convert_script(niftiVol,mgzVol,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT):
@@ -52,7 +56,7 @@ def run_mri_convert_script(niftiVol,mgzVol,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SC
     export FREESURFER_HOME={FSHOME}
     export SUBJECTS_DIR={FSSUBJDIR}
     source {SOURCE_SCRIPT}
-    {FSHOME}/bin/mri_convert --out_data_type uchar {invol} {outvol}
+    {FSHOME}/bin/mri_convert  --conform --out_data_type uchar {invol} {outvol}
 """.format(SOURCE_SCRIPT=FS_SCRIPT_FN,FSHOME=FREESURFER_HOME,FSSUBJDIR=FS_SUBJECTS_DIR,invol=niftiVol,outvol=mgzVol)
     script_name=mgzVol+'_convert.sh'
     script=open(script_name,'w')
@@ -64,6 +68,36 @@ def run_mri_convert_script(niftiVol,mgzVol,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SC
     print "Starting mri_convert"
     subprocess.check_call([script_name], stdout=script_name_stdout_fid, stderr=subprocess.STDOUT, shell='/bin/bash')
     print "Ending mri_convert"
+    script_name_stdout_fid.close()
+    return
+
+
+def run_mri_mask_script(output_brainmask_fn_mgz,output_custom_brainmask_fn_mgz, output_nu_fn_mgz,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT):
+    FS_SCRIPT_FN=os.path.join(FREESURFER_HOME,FS_SCRIPT)
+    mri_mask_script="""#!/bin/bash
+    export FREESURFER_HOME={FSHOME}
+    export SUBJECTS_DIR={FSSUBJDIR}
+    source {SOURCE_SCRIPT}
+    {FSHOME}/bin/mri_add_xform_to_header -c {CURRENT}/transforms/talairach.xfm {maskvol} {maskvol}
+    {FSHOME}/bin/mri_mask {invol} {maskvol} {outvol}
+""".format(SOURCE_SCRIPT=FS_SCRIPT_FN,
+           FSHOME=FREESURFER_HOME,
+           FSSUBJDIR=FS_SUBJECTS_DIR,
+           CURRENT=os.path.dirname(output_custom_brainmask_fn_mgz),
+           invol=output_nu_fn_mgz,
+           maskvol=output_custom_brainmask_fn_mgz,
+           # regmaskvol=output_custom_brainmask_fn_mgz.replace(".mgz", "_reg.mgz"),
+           outvol=output_brainmask_fn_mgz)
+    script_name=output_brainmask_fn_mgz+'_mask.sh'
+    script=open(script_name,'w')
+    script.write(mri_mask_script)
+    script.close()
+    os.chmod(script_name,0777)
+    script_name_stdout=output_brainmask_fn_mgz+'_convert.out'
+    script_name_stdout_fid=open(script_name_stdout,'w')
+    print "Starting mri_mask"
+    subprocess.check_call([script_name], stdout=script_name_stdout_fid, stderr=subprocess.STDOUT, shell='/bin/bash')
+    print "Ending mri_mask"
     script_name_stdout_fid.close()
     return
 
@@ -107,20 +141,19 @@ def baw_Recon1(t1_fn,wm_fn,brain_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT,su
         print "NOTHING TO BE DONE, SO SKIPPING."
         return # Nothing to be done, files are already up-to-date.
 
-def baw_FixBrainMask(wm_fn,brain_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT,subjID):
+def baw_FixBrainMask(brain_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT,subjID):
     base_subj_dir=os.path.join(FS_SUBJECTS_DIR,subjID,'mri')
     mkdir_p(base_subj_dir)
 
-    output_brainmask_fn=os.path.join(base_subj_dir,'brainmask.nii.gz')
-    output_nu_fn=os.path.join(base_subj_dir,'nu.nii.gz')
-
     output_brainmask_fn_mgz=os.path.join(base_subj_dir,'brainmask.mgz')
+    output_custom_brainmask_fn = os.path.join(base_subj_dir,'custom_brain_mask.nii.gz')
+    output_custom_brainmask_fn_mgz = os.path.join(base_subj_dir,'custom_brain_mask.mgz')
     output_nu_fn_mgz=os.path.join(base_subj_dir,'nu.mgz')
-    if True: #IsFirstNewerThanSecond(t1_fn,output_brainmask_fn_mgz):
+
+    if IsFirstNewerThanSecond(brain_fn,output_brainmask_fn_mgz) \
+        or IsFirstNewerThanSecond(brain_fn,output_nu_fn_mgz) \
+        or IsFirstNewerThanSecond(output_nu_fn_mgz,output_custom_brainmask_fn_mgz):
         print "Fixing BrainMask recon-auto1 stage"
-        run_mri_convert_script(output_nu_fn_mgz,output_nu_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
-        t1_new = sitk.ReadImage(output_nu_fn)
-        wm = sitk.ReadImage(wm_fn)
 
         brain = sitk.ReadImage(brain_fn)
         blood = sitk.BinaryThreshold(brain, 5, 5)
@@ -131,24 +164,31 @@ def baw_FixBrainMask(wm_fn,brain_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT,su
         ## some white matter is being classified as background when it it being avoid due
         ## to too strict of multi-modal thresholding.
         hole_filled=sitk.ErodeObjectMorphology(sitk.DilateObjectMorphology(clipping,fill_size),fill_size)
-        clipped = sitk.Cast(t1_new * hole_filled * not_blood,sitk.sitkUInt8)
+        final_mask = sitk.Cast(hole_filled * not_blood,sitk.sitkUInt8)
 
-        sitk.WriteImage(clipped,output_brainmask_fn) ## brain_matter image with values normalized 0-110, no skull or surface blood
+        ## Now make an mgz version of the binary custom brain mask
+        sitk.WriteImage(final_mask,output_custom_brainmask_fn) ## brain_matter mask with blood zero'ed out, and no "NOT" regions.
+        run_mri_convert_script(output_custom_brainmask_fn,output_custom_brainmask_fn_mgz,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
+
         os.rename(output_brainmask_fn_mgz, output_brainmask_fn_mgz.replace('.mgz','_orig_backup.mgz'))
-        run_mri_convert_script(output_brainmask_fn,output_brainmask_fn_mgz,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
+
+        ## Multipy output_brainmask_fn_mgz =  output_custom_brainmask_fn_mgz * output_nu_fn_mgz
+        run_mri_mask_script(output_brainmask_fn_mgz,output_custom_brainmask_fn_mgz, output_nu_fn_mgz,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
+
     else:
         print "NOTHING TO BE DONE, SO SKIPPING."
         return # Nothing to be done, files are already up-to-date.
 
 def runAutoReconStage(subjID,StageToRun,t1_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT):
     FS_SCRIPT_FN=os.path.join(FREESURFER_HOME,FS_SCRIPT)
-    base_subj_dir=os.path.join(FS_SUBJECTS_DIR,subjID,'mri')
-    orig_001_mgz_fn= os.path.join(base_subj_dir,'orig/001.mgz')
+    base_subj_dir=os.path.join(FS_SUBJECTS_DIR,subjID)
+    orig_001_mgz_fn= os.path.join(base_subj_dir,'mri','orig','001.mgz')
     init_flags=""
     if IsFirstNewerThanSecond(t1_fn,orig_001_mgz_fn):
-      mkdir_p(os.path.dirname(orig_001_mgz_fn))
-      run_mri_convert_script(t1_fn,orig_001_mgz_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
-      #init_flags=" -i {0} ".format(t1_fn)
+        if os.path.exists(base_subj_dir):
+            os.removedirs(base_subj_dir)
+        mkdir_p(os.path.dirname(orig_001_mgz_fn))
+        run_mri_convert_script(t1_fn,orig_001_mgz_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
 
     auto_recon_script="""#!/bin/bash
 export FREESURFER_HOME={FSHOME}
@@ -180,7 +220,7 @@ def runAutoRecon(t1_fn,wm_fn,brain_fn,subjID,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_
     """Run all stages of AutoRecon For Freesurfer, including the custom BAW initialization."""
 ## This did not work baw_Recon1(t1_fn,wm_fn,brain_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT,subjID)
     runAutoReconStage(subjID,1,t1_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
-#baw_FixBrainMask(wm_fn,brain_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT,subjID)
+    baw_FixBrainMask(brain_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT,subjID)
     runAutoReconStage(subjID,2,t1_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
     runAutoReconStage(subjID,3,t1_fn,FREESURFER_HOME,FS_SUBJECTS_DIR,FS_SCRIPT)
 

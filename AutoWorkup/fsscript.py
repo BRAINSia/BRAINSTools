1ee5084a2671ab645e47dbf03e233af3d2503237
David Welch <david.m.welch@gmail.com>
2012-12-19 11:20:58 -0600

    ENH: Adding template building

--- AutoWorkup/fsscript.py
+++ AutoWorkup/fsscript.py
@@ -2,6 +2,8 @@
 
 import argparse
 import os
+import shutil
+import SimpleITK as sitk
 import subprocess
 import SimpleITK as sitk
 
@@ -16,8 +18,7 @@ def normalizeWM(t1, wm_prob):
     wm_value = 1
     myMeasurementMap = ls.GetMeasurementMap(wm_value)
     MeanWM = myMeasurementMap['Mean']
-    t1_new = sitk.Cast(sitk.Cast(
-        t1, sitk.sitkFloat32) * WM_MEAN_FINAL / MeanWM, sitk.sitkUInt8)
+    t1_new = sitk.Cast(sitk.Cast(t1, sitk.sitkFloat32) * WM_MEAN_FINAL / MeanWM, sitk.sitkUInt8)
     return t1_new
 
 from PipeLineFunctionHelpers import mkdir_p
@@ -45,8 +46,11 @@ def run_mri_convert_script(niftiVol, mgzVol, FS_SUBJECTS_DIR, FREESURFER_HOME, F
     export FREESURFER_HOME={FSHOME}
     export SUBJECTS_DIR={FSSUBJDIR}
     source {SOURCE_SCRIPT}
-    {FSHOME}/bin/mri_convert  --conform --out_data_type uchar {invol} {outvol}
-""".format(SOURCE_SCRIPT=FS_SCRIPT_FN, FSHOME=FREESURFER_HOME, FSSUBJDIR=FS_SUBJECTS_DIR, invol=niftiVol, outvol=mgzVol)
+    {FSHOME}/bin/mri_convert --conform --out_data_type uchar {invol} {outvol}""".format(SOURCE_SCRIPT=FS_SCRIPT_FN,
+                                                                                         FSHOME=FREESURFER_HOME,
+                                                                                         FSSUBJDIR=FS_SUBJECTS_DIR,
+                                                                                         invol=niftiVol,
+                                                                                         outvol=mgzVol)
     script_name = mgzVol + '_convert.sh'
     script = open(script_name, 'w')
     script.write(mri_convert_script)
@@ -55,8 +59,7 @@ def run_mri_convert_script(niftiVol, mgzVol, FS_SUBJECTS_DIR, FREESURFER_HOME, F
     script_name_stdout = mgzVol + '_convert.out'
     script_name_stdout_fid = open(script_name_stdout, 'w')
     print "Starting mri_convert"
-    subprocess.check_call([script_name], stdout=script_name_stdout_fid,
-                          stderr=subprocess.STDOUT, shell='/bin/bash')
+    subprocess.check_call([script_name], stdout=script_name_stdout_fid, stderr=subprocess.STDOUT, shell='/bin/bash')
     print "Ending mri_convert"
     script_name_stdout_fid.close()
     return
@@ -76,8 +79,7 @@ def run_mri_mask_script(output_brainmask_fn_mgz, output_custom_brainmask_fn_mgz,
            CURRENT=os.path.dirname(output_custom_brainmask_fn_mgz),
            invol=output_nu_fn_mgz,
            maskvol=output_custom_brainmask_fn_mgz,
-           # regmaskvol=output_custom_brainmask_fn_mgz.replace(".mgz",
-           # "_reg.mgz"),
+           # regmaskvol=output_custom_brainmask_fn_mgz.replace(".mgz", "_reg.mgz"),
            outvol=output_brainmask_fn_mgz)
     script_name = output_brainmask_fn_mgz + '_mask.sh'
     script = open(script_name, 'w')
@@ -87,8 +89,7 @@ def run_mri_mask_script(output_brainmask_fn_mgz, output_custom_brainmask_fn_mgz,
     script_name_stdout = output_brainmask_fn_mgz + '_convert.out'
     script_name_stdout_fid = open(script_name_stdout, 'w')
     print "Starting mri_mask"
-    subprocess.check_call([script_name], stdout=script_name_stdout_fid,
-                          stderr=subprocess.STDOUT, shell='/bin/bash')
+    subprocess.check_call([script_name], stdout=script_name_stdout_fid, stderr=subprocess.STDOUT, shell='/bin/bash')
     print "Ending mri_mask"
     script_name_stdout_fid.close()
     return
@@ -115,8 +116,7 @@ def baw_Recon1(t1_fn, wm_fn, brain_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRI
         wm = sitk.ReadImage(wm_fn)
         t1_new = sitk.Cast(normalizeWM(t1, wm), sitk.sitkUInt8)
         sitk.WriteImage(t1_new, output_nu_fn)
-        run_mri_convert_script(output_nu_fn, output_nu_fn_mgz,
-                               FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+        run_mri_convert_script(output_nu_fn, output_nu_fn_mgz, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
 
         make_dummy_file(os.path.join(base_subj_dir, 'T1.mgz'))
         make_dummy_file(os.path.join(
@@ -144,19 +144,14 @@ def baw_Recon1(t1_fn, wm_fn, brain_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRI
 def baw_FixBrainMask(brain_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT, subject_id):
     base_subj_dir = os.path.join(FS_SUBJECTS_DIR, subject_id, 'mri')
     mkdir_p(base_subj_dir)
-
     output_brainmask_fn_mgz = os.path.join(base_subj_dir, 'brainmask.mgz')
-    output_custom_brainmask_fn = os.path.join(
-        base_subj_dir, 'custom_brain_mask.nii.gz')
-    output_custom_brainmask_fn_mgz = os.path.join(
-        base_subj_dir, 'custom_brain_mask.mgz')
+    output_custom_brainmask_fn = os.path.join(base_subj_dir, 'custom_brain_mask.nii.gz')
+    output_custom_brainmask_fn_mgz = os.path.join(base_subj_dir, 'custom_brain_mask.mgz')
     output_nu_fn_mgz = os.path.join(base_subj_dir, 'nu.mgz')
-
     if IsFirstNewerThanSecond(brain_fn, output_brainmask_fn_mgz) \
         or IsFirstNewerThanSecond(brain_fn, output_nu_fn_mgz) \
-            or IsFirstNewerThanSecond(output_nu_fn_mgz, output_custom_brainmask_fn_mgz):
+        or IsFirstNewerThanSecond(output_nu_fn_mgz, output_custom_brainmask_fn_mgz):
         print "Fixing BrainMask recon-auto1 stage"
-
         brain = sitk.ReadImage(brain_fn)
         blood = sitk.BinaryThreshold(brain, 5, 5)
         not_blood = 1 - blood
@@ -165,26 +160,17 @@ def baw_FixBrainMask(brain_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT, subj
         ## HACK: Unfortunately we need to hole fill because of a WM bug in BABC where
         ## some white matter is being classified as background when it it being avoid due
         ## to too strict of multi-modal thresholding.
-        hole_filled = sitk.ErodeObjectMorphology(
-            sitk.DilateObjectMorphology(clipping, fill_size), fill_size)
+        hole_filled = sitk.ErodeObjectMorphology(sitk.DilateObjectMorphology(clipping, fill_size), fill_size)
         final_mask = sitk.Cast(hole_filled * not_blood, sitk.sitkUInt8)
-
         ## Now make an mgz version of the binary custom brain mask
-        sitk.WriteImage(final_mask, output_custom_brainmask_fn)  # brain_matter mask with blood zero'ed out, and no "NOT" regions.
+        sitk.WriteImage(final_mask, output_custom_brainmask_fn) ## brain_matter mask with blood zero'ed out, and no "NOT" regions.
         run_mri_convert_script(output_custom_brainmask_fn, output_custom_brainmask_fn_mgz, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
-
-        os.rename(output_brainmask_fn_mgz,
-                  output_brainmask_fn_mgz.replace('.mgz', '_orig_backup.mgz'))
-
-        # Multipy output_brainmask_fn_mgz =  output_custom_brainmask_fn_mgz *
-        # output_nu_fn_mgz
+        os.rename(output_brainmask_fn_mgz, output_brainmask_fn_mgz.replace('.mgz','_orig_backup.mgz'))
+        ## Multipy output_brainmask_fn_mgz =  output_custom_brainmask_fn_mgz * output_nu_fn_mgz
         run_mri_mask_script(output_brainmask_fn_mgz, output_custom_brainmask_fn_mgz, output_nu_fn_mgz, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
-
     else:
         print "NOTHING TO BE DONE, SO SKIPPING."
         return  # Nothing to be done, files are already up-to-date.
-
-import shutil
 
 
 def removeDir(path):
@@ -195,28 +181,25 @@ def removeDir(path):
 def runAutoReconStage(subject_id, StageToRun, t1_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT):
     FS_SCRIPT_FN = os.path.join(FREESURFER_HOME, FS_SCRIPT)
     base_subj_dir = os.path.join(FS_SUBJECTS_DIR, subject_id)
-    orig_001_mgz_fn = os.path.join(base_subj_dir, 'mri', 'orig', '001.mgz')
+    orig_001_mgz_fn = os.path.join(base_subj_dir,'mri','orig','001.mgz')
     if IsFirstNewerThanSecond(t1_fn, orig_001_mgz_fn):
         if os.path.exists(base_subj_dir):
             removeDir(base_subj_dir)
         mkdir_p(os.path.dirname(orig_001_mgz_fn))
-        run_mri_convert_script(t1_fn, orig_001_mgz_fn,
-                               FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
-
-    auto_recon_script = """#!/bin/bash
-export FREESURFER_HOME={FSHOME}
-export SUBJECTS_DIR={FSSUBJDIR}
-source {SOURCE_SCRIPT}
-{FSHOME}/bin/recon-all -debug -subjid {SUBJID} -make autorecon{AUTORECONSTAGE}
-""".format(SOURCE_SCRIPT=FS_SCRIPT_FN,
-           FSHOME=FREESURFER_HOME,
-           FSSUBJDIR=FS_SUBJECTS_DIR,
-           AUTORECONSTAGE=StageToRun,
-           SUBJID=subject_id)
-    base_run_dir = os.path.join(FS_SUBJECTS_DIR, 'run_scripts', subject_id)
+        run_mri_convert_script(t1_fn, orig_001_mgz_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+    auto_recon_script="""#!/bin/bash
+        export FREESURFER_HOME={FSHOME}
+        export SUBJECTS_DIR={FSSUBJDIR}
+        source {SOURCE_SCRIPT}
+        {FSHOME}/bin/recon-all -debug -subjid {SUBJID} -make autorecon{AUTORECONSTAGE}
+    """.format(SOURCE_SCRIPT=FS_SCRIPT_FN,
+               FSHOME=FREESURFER_HOME,
+               FSSUBJDIR=FS_SUBJECTS_DIR,
+               AUTORECONSTAGE=StageToRun,
+               SUBJID=subject_id)
+    base_run_dir = os.path.join(FS_SUBJECTS_DIR,'run_scripts', subject_id)
     mkdir_p(base_run_dir)
-    script_name = os.path.join(
-        base_run_dir, 'run_autorecon_stage' + str(StageToRun) + '.sh')
+    script_name = os.path.join(base_run_dir,'run_autorecon_stage'+str(StageToRun)+'.sh')
     script = open(script_name, 'w')
     script.write(auto_recon_script)
     script.close()
@@ -224,8 +207,83 @@ source {SOURCE_SCRIPT}
     script_name_stdout = script_name + '_out'
     script_name_stdout_fid = open(script_name_stdout, 'w')
     print "Starting auto_recon Stage: {0} for SubjectSession {1}".format(StageToRun, subject_id)
-    subprocess.check_call([script_name], stdout=script_name_stdout_fid,
-                          stderr=subprocess.STDOUT, shell='/bin/bash')
+    subprocess.check_call([script_name], stdout=script_name_stdout_fid, stderr=subprocess.STDOUT, shell='/bin/bash')
+    print "Ending auto_recon Stage: {0} for SubjectSession {1}".format(StageToRun, subject_id)
+    script_name_stdout_fid.close()
+    return
+
+
+def runSubjectTemplate(subject_id, t1s, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT):
+    """ Create the within-subject template """
+    assert type(t1s, list) "Must input a list of T1s"
+    StageToRun = "Within-Subject Template"
+    FS_SCRIPT_FN = os.path.join(FREESURFER_HOME, FS_SCRIPT)
+    base_subj_dir = os.path.join(FS_SUBJECTS_DIR, subject_id)
+    ## orig_001_mgz_fn = os.path.join(base_subj_dir,'mri','orig','001.mgz')
+    ## if IsFirstNewerThanSecond(t1_fn, orig_001_mgz_fn):
+    ##     if os.path.exists(base_subj_dir):
+    ##         removeDir(base_subj_dir)
+    ##     mkdir_p(os.path.dirname(orig_001_mgz_fn))
+    ##     run_mri_convert_script(t1_fn, orig_001_mgz_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+    auto_recon_script="""#!/bin/bash
+    export FREESURFER_HOME={FSHOME}
+    export SUBJECTS_DIR={FSSUBJDIR}
+    source {SOURCE_SCRIPT}
+    {FSHOME}/bin/recon-all -debug -base {TEMPLATEID}""".format(SOURCE_SCRIPT=FS_SCRIPT_FN,
+                                                               FSHOME=FREESURFER_HOME,
+                                                               FSSUBJDIR=FS_SUBJECTS_DIR,
+                                                               TEMPLATEID=subject_id)
+    for t1 in t1s:
+        auto_recon_script += " -tp {timepoint}".format(timepoint=t1)
+    auto_recon_script += " -all" #TODO: Can we break this up???
+    base_run_dir = os.path.join(FS_SUBJECTS_DIR,'run_scripts', subject_id)
+    mkdir_p(base_run_dir)
+    script_name = os.path.join(base_run_dir,'run_autorecon_stage'+str(StageToRun)+'.sh')
+    script = open(script_name, 'w')
+    script.write(auto_recon_script)
+    script.close()
+    os.chmod(script_name, 0777)
+    script_name_stdout = script_name + '_out'
+    script_name_stdout_fid = open(script_name_stdout, 'w')
+    print "Starting auto_recon Stage: {0} for SubjectSession {1}".format(StageToRun, subject_id)
+    subprocess.check_call([script_name], stdout=script_name_stdout_fid, stderr=subprocess.STDOUT, shell='/bin/bash')
+    print "Ending auto_recon Stage: {0} for SubjectSession {1}".format(StageToRun, subject_id)
+    script_name_stdout_fid.close()
+    return
+
+
+def runLongitudinal(subject_id, t1, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT):
+    """ Create the longitudinal analysis """
+    assert type(t1, str) "Must input a list of T1s"
+    StageToRun = "Longitudinal"
+    FS_SCRIPT_FN = os.path.join(FREESURFER_HOME, FS_SCRIPT)
+    base_subj_dir = os.path.join(FS_SUBJECTS_DIR, subject_id)
+    ## orig_001_mgz_fn = os.path.join(base_subj_dir,'mri','orig','001.mgz')
+    ## if IsFirstNewerThanSecond(t1_fn, orig_001_mgz_fn):
+    ##     if os.path.exists(base_subj_dir):
+    ##         removeDir(base_subj_dir)
+    ##     mkdir_p(os.path.dirname(orig_001_mgz_fn))
+    ##     run_mri_convert_script(t1_fn, orig_001_mgz_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+    auto_recon_script="""#!/bin/bash
+    export FREESURFER_HOME={FSHOME}
+    export SUBJECTS_DIR={FSSUBJDIR}
+    source {SOURCE_SCRIPT}
+    {FSHOME}/bin/recon-all -debug -long {TIMEPOINT} {TEMPLATEID} -all""".format(SOURCE_SCRIPT=FS_SCRIPT_FN,
+                                                                                FSHOME=FREESURFER_HOME,
+                                                                                FSSUBJDIR=FS_SUBJECTS_DIR,
+                                                                                TEMPLATEID=subject_id,
+                                                                                TIMEPOINT=t1) #TODO: Can we break this up???
+    base_run_dir = os.path.join(FS_SUBJECTS_DIR,'run_scripts', subject_id)
+    mkdir_p(base_run_dir)
+    script_name = os.path.join(base_run_dir,'run_autorecon_stage'+str(StageToRun)+'.sh')
+    script = open(script_name, 'w')
+    script.write(auto_recon_script)
+    script.close()
+    os.chmod(script_name, 0777)
+    script_name_stdout = script_name + '_out'
+    script_name_stdout_fid = open(script_name_stdout, 'w')
+    print "Starting auto_recon Stage: {0} for SubjectSession {1}".format(StageToRun, subject_id)
+    subprocess.check_call([script_name], stdout=script_name_stdout_fid, stderr=subprocess.STDOUT, shell='/bin/bash')
     print "Ending auto_recon Stage: {0} for SubjectSession {1}".format(StageToRun, subject_id)
     script_name_stdout_fid.close()
     return
@@ -233,21 +291,22 @@ source {SOURCE_SCRIPT}
 
 def runAutoRecon(t1_fn, wm_fn, brain_fn, subject_id, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT):
     """Run all stages of AutoRecon For Freesurfer, including the custom BAW initialization."""
-# This did not work
-# baw_Recon1(t1_fn,wm_fn,brain_fn,FS_SUBJECTS_DIR,FREESURFER_HOME,FS_SCRIPT,subject_id)
-    runAutoReconStage(
-        subject_id, 1, t1_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
-    baw_FixBrainMask(
-        brain_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT, subject_id)
-    runAutoReconStage(
-        subject_id, 2, t1_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
-    runAutoReconStage(
-        subject_id, 3, t1_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+## This did not work baw_Recon1(t1_fn, wm_fn, brain_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT, subject_id)
+    runAutoReconStage(subject_id, 1, t1_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+    baw_FixBrainMask(brain_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT, subject_id)
+    runAutoReconStage(subject_id, 2, t1_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+    runAutoReconStage(subject_id, 3, t1_fn, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
     dirname = os.path.join(FS_SUBJECTS_DIR, subject_id)
     t1 = os.path.join(dirname, 'mri', 'brain.mgz')
     label1 = os.path.join(dirname, 'mri_nifti', 'aparc+aseg.nii.gz')
     label2 = os.path.join(dirname, 'mri_nifti', 'aparc.a2009+aseg.nii.gz')
     return t1, label1, label2
+
+def runLongitudinalAnalysis():
+    runSubjectTemplate(subject_id, t1s, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+    runLongitudinal(subject_id, t1, FS_SUBJECTS_DIR, FREESURFER_HOME, FS_SCRIPT)
+    dirname = os.path.join(FS_SUBJECTS_DIR, subject_id)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""
@@ -263,19 +322,16 @@ if __name__ == "__main__":
 
     """)
     # TODO: Make parser group "Inputs"
-    parser.add_argument(
-        '--T1image', action='store', dest='t1Image', help='Original T1 image')
-    # parser.add_argument('--T2image', action='store', dest='t2Image',
-    # help='Original T2 image')
-    parser.add_argument(
-        '--SubjID', action='store', dest='subject_id', help='Subject_Session')
+    flavor = parser.add_mutually_exclusive_group(required=True)
+    flavor.add_argument('--T1image', action='store', dest='t1Image', help='Original T1 image')
+    flavor.add_argument('--template', action='store_true', help="Use this flag to build the longitudinal subject template")
+    #parser.add_argument('--T2image', action='store', dest='t2Image', help='Original T2 image')
+    parser.add_argument('--SubjID', action='store', dest='subject_id', help='Subject_Session')
     parser.add_argument('--BrainLabel', action='store', dest='brainLabel',
                         help='The normalized T1 image with the skull removed. Normalized 0-110 where white matter=110.')
-    parser.add_argument(
-        '--WMProb', action='store', dest='wmProbImage', help='')
+    parser.add_argument('--WMProb', action='store', dest='wmProbImage', help='')
     # TODO: Make parser group "Environment"
-    parser.add_argument('--FSSubjDir', action='store', dest='FS_SUBJECTS_DIR',
-                        help='FreeSurfer subjects directory')
+    parser.add_argument('--FSSubjDir', action='store', dest='FS_SUBJECTS_DIR', help='FreeSurfer subjects directory')
 ## For the current nipype processing, the environments are set prior to running this script, so this code is not
 ## needed for using this script within the baw running envirionment.
 # TODO: parser.add_argument('--FSHomeDir', action='store', dest='FREESURFER_HOME',
@@ -283,21 +339,22 @@ if __name__ == "__main__":
 # TODO:                  help='Location of FreeSurfer (differs for Mac and Linux environments')
 # TODO: parser.add_argument('--FSSource', action='store', dest='FS_SCRIPT',
 # TODO:                  default='FreeSurferEnv.sh', help='')
-# TODO: local_FREESURFER_HOME=all_args.FREESURFER_HOME
-# TODO: local_FS_SCRIPT=all_args.FS_SCRIPT
+# TODO: local_FREESURFER_HOME = all_args.FREESURFER_HOME
+# TODO: local_FS_SCRIPT = all_args.FS_SCRIPT
 
     all_args = parser.parse_args()
     local_FREESURFER_HOME = os.environ['FREESURFER_HOME']
     if not os.path.exists(local_FREESURFER_HOME):
-        raise Exception("INVALID PATH FOR FREESURFER HOME :{0}:".format(
-            local_FREESURFER_HOME))
-    local_FS_SCRIPT = os.path.join(local_FREESURFER_HOME, 'FreeSurferEnv.sh')
+        raise Exception("INVALID PATH FOR FREESURFER HOME :{0}:".format(local_FREESURFER_HOME))
+    local_FS_SCRIPT = os.path.join(local_FREESURFER_HOME,'FreeSurferEnv.sh')
     local_FS_SCRIPT = 'FreeSurferEnv.sh'
-
-    runAutoRecon(all_args.t1Image,
-                 all_args.wmProbImage,
-                 all_args.brainLabel,
-                 all_args.subject_id,
-                 all_args.FS_SUBJECTS_DIR,
-                 local_FREESURFER_HOME,
-                 local_FS_SCRIPT)
+    if all_args.t1Image is None: ### TODO: Verify that NONE is returned!
+       runSubjectTemplate()
+    else:
+        runAutoRecon(all_args.t1Image,
+                     all_args.wmProbImage,
+                     all_args.brainLabel,
+                     all_args.subject_id,
+                     all_args.FS_SUBJECTS_DIR,
+                     local_FREESURFER_HOME,
+                     local_FS_SCRIPT)
--- AutoWorkup/fswrap.py
+++ AutoWorkup/fswrap.py
@@ -9,26 +9,23 @@ from nipype.interfaces.base import CommandLine, CommandLineInputSpec
 
 
 class FSScriptInputSpec(CommandLineInputSpec):
-    T1_files = File(
-        argstr='--T1image %s', exists=True, desc='Original T1 image')
+    T1_files = File(argstr='--T1image %s', exists=True, desc='Original T1 image', xor=['buildTemplate'])
     brainmask = File(argstr='--BrainLabel %s', exists=True,
                      desc='The normalized T1 image with the skull removed. Normalized 0-110 where white matter=110.')
     wm_prob = File(argstr='--WMProb %s', exists=True, desc='')
     subject_id = traits.Str(argstr='--SubjID %s', desc='Subject_Session')
-    subjects_dir = Directory(
-        argstr='--FSSubjDir %s', desc='FreeSurfer subjects directory')
-# TODO:    fs_env_script = traits.Str(argstr='--FSSource %s', default='${FREESURFER_HOME}/FreeSurferEnv.sh',
-# TODO:                           desc='')
-# TODO:    fs_home = Directory(argstr='--FSHomeDir %s',
-# TODO:                         desc='Location of FreeSurfer (differs for
-# Mac and Linux environments')
-
+    subjects_dir = Directory(argstr='--FSSubjDir %s', desc='FreeSurfer subjects directory')
+    buildTemplate = traits.Bool(False, argstr='--template', usedefault=True, desc='Toggle use of longitudinal subject template build', xor=['T1_files'])
+#TODO:    fs_env_script = traits.Str(argstr='--FSSource %s', default='${FREESURFER_HOME}/FreeSurferEnv.sh',
+#TODO:                           desc='')
+#TODO:    fs_home = Directory(argstr='--FSHomeDir %s',
+#TODO:                         desc='Location of FreeSurfer (differs for Mac and Linux environments')
 
 class FSScriptOutputSpec(TraitedSpec):
     T1_out = File(exist=True, desc='brain.mgz')
     label1_out = File(exist=True, desc='aparc+aseg.nii.gz')
     label2_out = File(exist=True, desc='aparc.a2009+aseg.nii.gz')
-
+    template = File(exist=True, desc='longitudinal subject template')
 
 class FSScript(CommandLine):
     """
@@ -43,17 +40,18 @@ class FSScript(CommandLine):
     this_path = os.path.dirname(
         os.path.abspath(inspect.getfile(inspect.currentframe())))  # script)
 
-    _cmd = '{0} {1}'.format(
-        sys.executable, os.path.join(this_path, 'fsscript.py'))
-# _cmd = 'fsscript.py'
+    _cmd = '{0} {1}'.format(sys.executable, os.path.join(this_path,'fsscript.py'))
+#_cmd = 'fsscript.py'
     input_spec = FSScriptInputSpec
     output_spec = FSScriptOutputSpec
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['T1_out'] = os.path.join(os.getcwd(), 'mri', 'brain.mgz')
-        outputs['label1_out'] = os.path.join(
-            os.getcwd(), 'mri_nifti', 'aparc+aseg.nii.gz')
-        outputs['label2_out'] = os.path.join(
-            os.getcwd(), 'mri_nifti', 'aparc.a2009+aseg.nii.gz')
+        if isdefined(self.inputs.T1_files):
+            outputs['T1_out'] = os.path.join(os.getcwd(), 'mri', 'brain.mgz')
+            outputs['label1_out'] = os.path.join(os.getcwd(), 'mri_nifti', 'aparc+aseg.nii.gz')
+            outputs['label2_out'] = os.path.join(os.getcwd(), 'mri_nifti', 'aparc.a2009+aseg.nii.gz')
+        else:
+            subject = self.inputs.subject_id.split("_")[0]
+            outputs['template'] = os.path.join(os.getcwd(), subject + "_template", templateFile) # TODO: templateFile!!!
         return outputs

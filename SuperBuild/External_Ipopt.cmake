84db393f74f6bec1624ce363b65e731171bd1c03
Kent Williams <norman-k-williams@uiowa.edu>
2013-03-01 08:56:11 -0600

    COMP: Add Iopt

---
+++ SuperBuild/External_Ipopt.cmake
@@ -0,0 +1,104 @@
+# Make sure this file is included only once by creating globally unique varibles
+# based on the name of this included file.
+get_filename_component(CMAKE_CURRENT_LIST_FILENAME ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
+if(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED)
+  return()
+endif()
+set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
+
+## External_${extProjName}.cmake files can be recurisvely included,
+## and cmake variables are global, so when including sub projects it
+## is important make the extProjName and proj variables
+## appear to stay constant in one of these files.
+## Store global variables before overwriting (then restore at end of this file.)
+ProjectDependancyPush(CACHED_extProjName ${extProjName})
+ProjectDependancyPush(CACHED_proj ${proj})
+
+# Make sure that the ExtProjName/IntProjName variables are unique globally
+# even if other External_${ExtProjName}.cmake files are sourced by
+# SlicerMacroCheckExternalProjectDependency
+set(extProjName Ipopt) #The find_package known name
+set(proj        Ipopt) #This local name
+set(${extProjName}_REQUIRED_VERSION "")  #If a required version is necessary, then set this, else leave blank
+
+#if(${USE_SYSTEM_${extProjName}})
+#  unset(${extProjName}_DIR CACHE)
+#endif()
+
+# Sanity checks
+if(DEFINED ${extProjName}_DIR AND NOT EXISTS ${${extProjName}_DIR})
+  message(FATAL_ERROR "${extProjName}_DIR variable is defined but corresponds to non-existing directory (${${extProjName}_DIR})")
+endif()
+
+# Set dependency list
+set(${proj}_DEPENDENCIES "")
+
+# Include dependent projects if any
+SlicerMacroCheckExternalProjectDependency(${proj})
+
+if(NOT ( DEFINED "${extProjName}_DIR" OR ( DEFINED "${USE_SYSTEM_${extProjName}}" AND NOT "${USE_SYSTEM_${extProjName}}" ) ) )
+  #message(STATUS "${__indent}Adding project ${proj}")
+
+  # Set CMake OSX variable to pass down the external project
+  set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
+  if(APPLE)
+    list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
+      -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+      -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+  endif()
+
+  ### --- Project specific additions here
+  set(${proj}_CMAKE_OPTIONS
+      -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/${proj}-install
+      -DBUILD_EXAMPLES:BOOL=OFF
+      -DBUILD_TESTING:BOOL=OFF
+      -DBUILD_TESTS:BOOL=OFF
+      -DBUILD_SHARED_LIBS:BOOL=OFF
+  )
+
+  set(${proj}_binary_dir ${CMAKE_CURRENT_BINARY_DIR}/${proj}-build)
+  set(${proj}_source_dir ${CMAKE_CURRENT_BINARY_DIR}/${proj})
+  set(${proj}_install_dir ${CMAKE_CURRENT_BINARY_DIR}/${proj}-install)
+  set(${proj}_configure_script
+    ${CMAKE_CURRENT_BINARY_DIR}/External_Ipopt_configure_step.cmake)
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/SuperBuild/External_Ipopt_configure_step.cmake.in
+    ${${proj}_configure_script}
+    @ONLY)
+
+  set(${proj}_CONFIGURE_COMMAND ${CMAKE_COMMAND}
+    -P ${${proj}_configure_script})
+
+  ### --- End Project specific additions
+  set(${proj}_REPOSITORY "https://projects.coin-or.org/svn/Ipopt/trunk") # USE THIS FOR UPDATED VERSION
+  set(${proj}_GIT_TAG "r2163")
+  ExternalProject_Add(${proj}
+    SVN_REPOSITORY ${${proj}_REPOSITORY}
+    SVN_REVISION -r ${${proj}_GIT_TAG}
+    SVN_TRUST_CERT 1
+    SOURCE_DIR ${proj}
+    BINARY_DIR ${proj}-build
+    INSTALL_DIR ${proj}-install
+    ${cmakeversion_external_update} "${cmakeversion_external_update_value}"
+    CONFIGURE_COMMAND ${${proj}_CONFIGURE_COMMAND}
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+  )
+  set(${extProjName}_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+else()
+  if(${USE_SYSTEM_${extProjName}})
+    find_package(${extProjName} ${${extProjName}_REQUIRED_VERSION} REQUIRED)
+    if(NOT ${extProjName}_DIR)
+      message(FATAL_ERROR "To use the system ${extProjName}, set ${extProjName}_DIR")
+    endif()
+    message("USING the system ${extProjName}, set ${extProjName}_DIR=${${extProjName}_DIR}")
+  endif()
+  # The project is provided using ${extProjName}_DIR, nevertheless since other
+  # project may depend on ${extProjName}, let's add an 'empty' one
+  SlicerMacroEmptyExternalProject(${proj} "${${proj}_DEPENDENCIES}")
+endif()
+
+list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS ${extProjName}_DIR:PATH)
+
+ProjectDependancyPop(CACHED_extProjName extProjName)
+ProjectDependancyPop(CACHED_proj proj)

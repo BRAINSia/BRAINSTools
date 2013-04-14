13b6fe7c510c7f288b2ba00af5311ee31c6b32d5
Hans Johnson <hans-johnson@uiowa.edu>
2013-04-13 22:59:16 -0500

    ENH: Update ANTS for Boost build
    ANTS boost build was building everything and
    that sometimes would cause failures on
    platforms.  Since very little is needed, only
    the minimum part of ANTS that is need
    is built.

--- SuperBuild/External_ANTS.cmake
+++ SuperBuild/External_ANTS.cmake
@@ -7,7 +7,7 @@ set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
 
 set(proj ANTS)
 set(${proj}_GIT_REPOSITORY "git://github.com/stnava/ANTs.git")
-set(${proj}_GIT_TAG "64ae5063784a844405f63ec966f666759b5e44b0") #2013-01-01 Include antsApplyTransformsToPoints.
+set(${proj}_GIT_TAG "4ecfca3124c4ea53e057bd11b89254636d81257c") #2013-04-13 update for better boost building.
 
 set(${proj}_DEPENDENCIES ${ITK_EXTERNAL_NAME} SlicerExecutionModel)
 

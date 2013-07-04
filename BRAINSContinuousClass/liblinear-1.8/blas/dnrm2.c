b67f6bdd0a5fab356571ba48c6e21371c60ba9ab
Hans Johnson <hans-johnson@uiowa.edu>
2013-07-04 14:18:08 -0500

    COMP: Minor c-style compilation error.

--- BRAINSContinuousClass/liblinear-1.8/blas/dnrm2.c
+++ BRAINSContinuousClass/liblinear-1.8/blas/dnrm2.c
@@ -6,13 +6,13 @@ double dnrm2_(int *n, double *x, int *incx)
   long int nn, iincx;
   double norm;
 
-/*  DNRM2 returns the euclidean norm of a vector via the function   
-    name, so that   
+/*  DNRM2 returns the euclidean norm of a vector via the function
+    name, so that
 
-       DNRM2 := sqrt( x'*x )   
+       DNRM2 := sqrt( x'*x )
 
-    -- This version written on 25-October-1982.   
-       Modified on 14-October-1993 to inline the call to SLASSQ.   
+    -- This version written on 25-October-1982.
+       Modified on 14-October-1993 to inline the call to SLASSQ.
        Sven Hammarling, Nag Ltd.   */
 
   /* Dereference inputs */
@@ -24,16 +24,18 @@ double dnrm2_(int *n, double *x, int *incx)
     if (nn == 1)
     {
       norm = fabs(x[0]);
-    }  
+    }
     else
     {
       long int scale = 0.0;
       long int ssq = 1.0;
 
-      /* The following loop is equivalent to this call to the LAPACK 
+      /* The following loop is equivalent to this call to the LAPACK
          auxiliary routine:   CALL SLASSQ( N, X, INCX, SCALE, SSQ ) */
 
-      for (long int ix=(nn-1)*iincx; ix>=0; ix-=iincx)
+      {
+      long int ix;
+      for (ix=(nn-1)*iincx; ix>=0; ix-=iincx)
       {
         if (x[ix] != 0.0)
         {
@@ -51,6 +53,7 @@ double dnrm2_(int *n, double *x, int *incx)
             ssq += temp * temp;
           }
         }
+      }
       }
       norm = scale * sqrt(ssq);
     }

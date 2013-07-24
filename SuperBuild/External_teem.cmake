2c2ed48242d5c647772411bbc0f1d1eb94d96b7a
Kent Williams <norman-k-williams@uiowa.edu>
2013-07-24 12:11:37 -0500

    COMP: synchronized External_teem.cmake with NAMICExtProjects

--- SuperBuild/External_teem.cmake
+++ SuperBuild/External_teem.cmake
@@ -1,71 +1,59 @@
-
-# Make sure this file is included only once
+# Make sure this file is included only once by creating globally unique varibles
+# based on the name of this included file.
 get_filename_component(CMAKE_CURRENT_LIST_FILENAME ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
 if(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED)
   return()
 endif()
 set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
 
+## External_${extProjName}.cmake files can be recurisvely included,
+## and cmake variables are global, so when including sub projects it
+## is important make the extProjName and proj variables
+## appear to stay constant in one of these files.
+## Store global variables before overwriting (then restore at end of this file.)
+ProjectDependancyPush(CACHED_extProjName ${extProjName})
+ProjectDependancyPush(CACHED_proj ${proj})
+
+# Make sure that the ExtProjName/IntProjName variables are unique globally
+# even if other External_${ExtProjName}.cmake files are sourced by
+# SlicerMacroCheckExternalProjectDependency
+set(extProjName Teem) #The find_package known name
+set(proj        teem) #This local name
+set(${extProjName}_REQUIRED_VERSION zlib VTK)  #If a required version is necessary, then set this, else leave blank
+
+#if(${USE_SYSTEM_${extProjName}})
+#  unset(${extProjName}_DIR CACHE)
+#endif()
+
+# Sanity checks
+if(DEFINED ${extProjName}_DIR AND NOT EXISTS ${${extProjName}_DIR})
+  message(FATAL_ERROR "${extProjName}_DIR variable is defined but corresponds to non-existing directory (${${extProjName}_DIR})")
+endif()
+
 # Set dependency list
-set(teem_DEPENDENCIES zlib VTK)
+set(${proj}_DEPENDENCIES "")
+#if(${PROJECT_NAME}_BUILD_DICOM_SUPPORT)
+#  list(APPEND ${proj}_DEPENDENCIES DCMTK)
+#endif()
 
 # Include dependent projects if any
-SlicerMacroCheckExternalProjectDependency(teem)
-set(proj teem)
+SlicerMacroCheckExternalProjectDependency(${proj})
 
-set(EXTERNAL_PROJECT_OPTIONAL_ARGS)
+if(NOT ( DEFINED "${extProjName}_DIR" OR ( DEFINED "${USE_SYSTEM_${extProjName}}" AND NOT "${USE_SYSTEM_${extProjName}}" ) ) )
+  #message(STATUS "${__indent}Adding project ${proj}")
 
-# Set CMake OSX variable to pass down the external project
-if(APPLE)
-  list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
-    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
-    -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
-endif()
+  # Set CMake OSX variable to pass down the external project
+  set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
+  if(APPLE)
+    list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
+      -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+      -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+  endif()
 
-if(NOT CMAKE_CONFIGURATION_TYPES)
-  list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE})
-endif()
-
-set(CMAKE_PROJECT_INCLUDE_EXTERNAL_PROJECT_ARG)
-if(CTEST_USE_LAUNCHERS)
-  set(CMAKE_PROJECT_INCLUDE_EXTERNAL_PROJECT_ARG
-    "-DCMAKE_PROJECT_Teem_INCLUDE:FILEPATH=${CMAKE_ROOT}/Modules/CTestUseLaunchers.cmake")
-endif()
-
-#message(STATUS "${__indent}Adding project ${proj}")
-
-set(teem_PNG_LIBRARY_DIR ${VTK_DIR}/bin)
-if(CMAKE_CONFIGURATION_TYPES)
-  set(teem_PNG_LIBRARY_DIR ${teem_PNG_LIBRARY_DIR}/${CMAKE_CFG_INTDIR})
-endif()
-if(WIN32)
-  set(teem_PNG_LIBRARY ${teem_PNG_LIBRARY_DIR}/vtkpng.lib)
-elseif(APPLE)
-  set(teem_PNG_LIBRARY ${teem_PNG_LIBRARY_DIR}/libvtkpng.dylib)
-else()
-  set(teem_PNG_LIBRARY ${teem_PNG_LIBRARY_DIR}/libvtkpng.so)
-endif()
-
-set(teem_URL http://svn.slicer.org/Slicer3-lib-mirrors/trunk/teem-1.10.0-src.tar.gz)
-set(teem_MD5 efe219575adc89f6470994154d86c05b)
-
-ExternalProject_Add(${proj}
-  URL ${teem_URL}
-  URL_MD5 ${teem_MD5}
-  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCE_DIR teem
-  BINARY_DIR teem-build
-  CMAKE_GENERATOR ${gen}
-  CMAKE_ARGS
-    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-    # Not needed -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
-    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
-    -DBUILD_TESTING:BOOL=OFF
-    -DBUILD_SHARED_LIBS:BOOL=ON
-    ${CMAKE_PROJECT_INCLUDE_EXTERNAL_PROJECT_ARG}
+  ### --- Project specific additions here
+  set(${proj}_CMAKE_OPTIONS
+    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/${proj}-install
     -DTeem_USE_LIB_INSTALL_SUBDIR:BOOL=ON
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
     -DTeem_PTHREAD:BOOL=OFF
@@ -73,16 +61,48 @@ ExternalProject_Add(${proj}
     -DTeem_ZLIB:BOOL=ON
     -DTeem_PNG:BOOL=OFF
     -DZLIB_ROOT:PATH=${SLICER_ZLIB_ROOT}
-    -DZLIB_INCLUDE_DIR:PATH=${SLICER_ZLIB_INCLUDE_DIR}
-    -DZLIB_LIBRARY:FILEPATH=${SLICER_ZLIB_LIBRARY}
-    -DTeem_VTK_MANGLE:BOOL=OFF ## NOT NEEDED FOR EXTERNAL ZLIB outside of vtk
-#-DPNG_PNG_INCLUDE_DIR:PATH=${VTK_SOURCE_DIR}/Utilities/vtkpng
-#-DTeem_PNG_DLLCONF_IPATH:PATH=${VTK_DIR}/Utilities
-#-DPNG_LIBRARY:FILEPATH=${teem_PNG_LIBRARY}
-    ${EXTERNAL_PROJECT_OPTIONAL_ARGS}
-  INSTALL_COMMAND ""
-  DEPENDS
-    ${teem_DEPENDENCIES}
-  )
+    -DZLIB_INCLUDE_DIR:PATH=${ZLIB_INCLUDE_DIR}
+    -DZLIB_LIBRARY:FILEPATH=${ZLIB_LIBRARY}
+    -DTeem_VTK_MANGLE:BOOL=OFF
+    )
 
-set(Teem_DIR ${CMAKE_BINARY_DIR}/teem-build)
+  ### --- End Project specific additions
+  set(${proj}_URL http://svn.slicer.org/Slicer3-lib-mirrors/trunk/teem-1.10.0-src.tar.gz)
+  set(${proj}_MD5 efe219575adc89f6470994154d86c05b)
+  ExternalProject_Add(${proj}
+    URL ${${proj}_URL}
+    URL_MD5 ${${proj}_MD5}
+    SOURCE_DIR ${proj}
+    BINARY_DIR ${proj}-build
+    LOG_CONFIGURE 0  # Wrap configure in script to ignore log output from dashboards
+    LOG_BUILD     0  # Wrap build in script to to ignore log output from dashboards
+    LOG_TEST      0  # Wrap test in script to to ignore log output from dashboards
+    LOG_INSTALL   0  # Wrap install in script to to ignore log output from dashboards
+    ${cmakeversion_external_update} "${cmakeversion_external_update_value}"
+    CMAKE_GENERATOR ${gen}
+    CMAKE_ARGS
+      ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
+      ${COMMON_EXTERNAL_PROJECT_ARGS}
+      ${${proj}_CMAKE_OPTIONS}
+## We really do want to install in order to limit # of include paths INSTALL_COMMAND ""
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+  )
+  set(${extProjName}_DIR ${CMAKE_BINARY_DIR}/${proj}-install/lib/Teem-1.10.0)
+else()
+  if(${USE_SYSTEM_${extProjName}})
+    find_package(${extProjName} ${${extProjName}_REQUIRED_VERSION} REQUIRED)
+    if(NOT ${extProjName}_DIR)
+      message(FATAL_ERROR "To use the system ${extProjName}, set ${extProjName}_DIR")
+    endif()
+    message("USING the system ${extProjName}, set ${extProjName}_DIR=${${extProjName}_DIR}")
+  endif()
+  # The project is provided using ${extProjName}_DIR, nevertheless since other
+  # project may depend on ${extProjName}, let's add an 'empty' one
+  SlicerMacroEmptyExternalProject(${proj} "${${proj}_DEPENDENCIES}")
+endif()
+
+list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS ${extProjName}_DIR:PATH)
+
+ProjectDependancyPop(CACHED_extProjName extProjName)
+ProjectDependancyPop(CACHED_proj proj)

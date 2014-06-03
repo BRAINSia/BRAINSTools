c071f92dd2003250d581841a4f59851ee6ec9ee7
Hans Johnson <hans-johnson@uiowa.edu>
2014-06-03 15:57:52 -0500

    COMP: Missed update of ITKv4 sources.

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   endif()
 
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  set(${proj}_GIT_TAG 5e2b1bfee965a5a4f0dfdf3f7eefbc79989adce8)
+  set(${proj}_GIT_TAG c8e438d7ccd47a9131b0ee785d1a13184cd6e91d)
   message("COMMON_EXTERNAL_PROJECT_ARGS:
 ${COMMON_EXTERNAL_PROJECT_ARGS}")
   ExternalProject_Add(${proj}

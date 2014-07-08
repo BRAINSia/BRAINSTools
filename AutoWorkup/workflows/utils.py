5e6a65db3e453741ed4dfc37cddae3eb2d2cb140
David Welch <david.m.welch@gmail.com>
2014-07-08 13:29:20 -0500

    STYLE: print_workflow documentation and assertion

--- AutoWorkup/workflows/utils.py
+++ AutoWorkup/workflows/utils.py
@@ -8,10 +8,6 @@ def run_workflow(workflow, plugin='Linear', plugin_args={}):
     print "Running workflow..."
     try:
         workflow.run(plugin_args=plugin_args)
-        # if not plugin in ['Linear', 'MultiProc']:
-        #     workflow.run(plugin_args=plugin_args)
-        # else:
-        #     workflow.run()
     except:
         print "=+-+" * 25
         print "Error: Exception while running subjects"
@@ -19,12 +15,18 @@ def run_workflow(workflow, plugin='Linear', plugin_args={}):
         return False
     return True
 
-def print_workflow(workflow, plugin, dotfilename='workflow', graph2use='hierarchial'):
-    if plugin in ['Linear', 'MultiProc']:
-        print "Writing graph to filename {0}".format(dotfilename)
-        try:
-            workflow.write_graph(dotfilename=dotfilename, graph2use=graph2use)
-            return True
-        except:
-            raise
+
+def print_workflow(workflow, plugin, dotfilename='workflow', graph2use='hierarchical'):
+    """
+    HINT: graph2use values: ['orig', 'flat', 'hierarchical', 'exec']
+    'hierarchical' is the only one that DOES NOT require pygraphviz
+    """
+    assert plugin in ['Linear', 'MultiProc'], "'plugin' must be in ['Linear', 'MultiProc'] to print workflow"
+    dotfilename = '_'.join([dotfilename, graph2use])
+    print "Writing graph to filename {0}".format(dotfilename)
+    try:
+        workflow.write_graph(dotfilename=dotfilename, graph2use=graph2use)
+        return True
+    except:
+        raise
     return False

68e08db89eed8caecf43787e2f24d4555b76b6a0
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2015-04-02 09:00:43 -0500

    BUG: Avoid dtiprocess to stop nipype processing
    In the linux, "dtiprocess" returns a segmentation fault after it
    finishes all its processing at the moment of destroying all registered
    factories.
    This patch runs "dtiprocess" through an utility function to ignore this
    probable exception. However, it runs some checking to make sure all
    needed outputs of dtiprocess are written properly.

--- AutoWorkup/DWIProcessingWorkflows/EstimationWorkflow.py
+++ AutoWorkup/DWIProcessingWorkflows/EstimationWorkflow.py
@@ -16,6 +16,38 @@ import nipype.pipeline.engine as pe  # pypeline engine
 from SEMTools import *
 
 def CreateEstimationWorkflow(WFname):
+    #### Utility function ####
+    def RunDTIProcess(dti_image):
+        import os
+        from SEMTools import dtiprocess
+        DTIProcess = dtiprocess()
+        DTIProcess.inputs.dti_image = dti_image
+        DTIProcess.inputs.fa_output = 'FA.nrrd'
+        DTIProcess.inputs.md_output = 'MD.nrrd'
+        DTIProcess.inputs.RD_output = 'RD.nrrd'
+        DTIProcess.inputs.frobenius_norm_output = 'frobenius_norm_output.nrrd'
+        DTIProcess.inputs.lambda1_output = 'lambda1_output.nrrd'
+        DTIProcess.inputs.lambda2_output = 'lambda2_output.nrrd'
+        DTIProcess.inputs.lambda3_output = 'lambda3_output.nrrd'
+        DTIProcess.inputs.scalar_float = True
+        DTIProcess.inputs.ignore_exception = True
+        DTIProcess.run()
+        fa_output = os.path.join(os.getcwd(), 'FA.nrrd')
+        md_output = os.path.join(os.getcwd(), 'MD.nrrd')
+        RD_output = os.path.join(os.getcwd(), 'RD.nrrd')
+        frobenius_norm_output = os.path.join(os.getcwd(), 'frobenius_norm_output.nrrd')
+        lambda1_output = os.path.join(os.getcwd(), 'lambda1_output.nrrd')
+        lambda2_output = os.path.join(os.getcwd(), 'lambda2_output.nrrd')
+        lambda3_output = os.path.join(os.getcwd(), 'lambda3_output.nrrd')
+        assert os.path.isfile(fa_output), "FA file is not found: %s" % fa_output
+        assert os.path.isfile(md_output), "MD file is not found: %s" % md_output
+        assert os.path.isfile(RD_output), "RD file is not found: %s" % RD_output
+        assert os.path.isfile(frobenius_norm_output), "frobenius_norm file is not found: %s" % frobenius_norm_output
+        assert os.path.isfile(lambda1_output), "lambda1 file is not found: %s" % lambda1_output
+        assert os.path.isfile(lambda2_output), "lambda2 file is not found: %s" % lambda2_output
+        assert os.path.isfile(lambda3_output), "lambda3 file is not found: %s" % lambda3_output
+        return fa_output,md_output,RD_output,frobenius_norm_output,lambda1_output,lambda2_output,lambda3_output
+    #########################
 
     EstimationWF = pe.Workflow(name=WFname)
 
@@ -37,6 +69,16 @@ def CreateEstimationWorkflow(WFname):
     EstimationWF.connect(DTIEstim, 'tensor_output', outputsSpec, 'tensor_image')
 
     # Step2: DTI process
+    # HACK: In the linux, "dtiprocess" returns a segmentation fault after it finishes all its processing
+    # at the moment of destroying all registered factories.
+    # Therefore, we run "dtiprocess" through an utility function to ignore this probable exception.
+    # However, some checkings are done to make sure all needed outputs of dtiprocess are written properly.
+    DTIProcess = pe.Node(interface=Function(function = RunDTIProcess,
+                                 input_names=['dti_image'],
+                                 output_names=['fa_output','md_output','RD_output','frobenius_norm_output',
+                                               'lambda1_output','lambda2_output','lambda3_output']),
+                         name="DTIProcess")
+    '''
     DTIProcess = pe.Node(interface=dtiprocess(), name='DTIProcess')
     DTIProcess.inputs.fa_output = 'FA.nrrd'
     DTIProcess.inputs.md_output = 'MD.nrrd'
@@ -46,7 +88,7 @@ def CreateEstimationWorkflow(WFname):
     DTIProcess.inputs.lambda2_output = 'lambda2_output.nrrd'
     DTIProcess.inputs.lambda3_output = 'lambda3_output.nrrd'
     DTIProcess.inputs.scalar_float = True
-
+    '''
     EstimationWF.connect(DTIEstim, 'tensor_output', DTIProcess, 'dti_image')
     EstimationWF.connect(DTIProcess, 'fa_output', outputsSpec, 'FAImage')
     EstimationWF.connect(DTIProcess, 'md_output', outputsSpec, 'MDImage')

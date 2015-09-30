2ac44f2ab893915a5f4be2264164eb5b4812c2a2
Jessica Forbes <jessica-forbes@uiowa.edu>
2015-09-30 10:56:23 -0500

    ENH: Includes new dilation arg for dust cleanup in pipeline

--- AutoWorkup/workflows/WorkupAtlasDustCleanup.py
+++ AutoWorkup/workflows/WorkupAtlasDustCleanup.py
@@ -54,8 +54,8 @@ def CreateDustCleanupWorkflow(WFname, onlyT1, master_config,
     sessionMakeMultimodalInput = pe.Node(Function(function=MakeVector,
                                                   input_names=['inFN1', 'inFN2', 'inAtlas', 'outAtlas',
                                                                'maxIslandCount', 'useFullyConnected',
-                                                               'forceLabelChange', 'includeList',
-                                                               'excludeList'],
+                                                               'forceLabelChange', 'noDilation',
+                                                               'includeList', 'excludeList'],
                                                   output_names=['arguments']),
                                 run_without_submitting=True, name="sessionMakeMultimodalInput")
     dustCleanupWF.connect(inputsSpec, 'subj_t1_image', sessionMakeMultimodalInput, 'inFN1')
@@ -69,6 +69,7 @@ def CreateDustCleanupWorkflow(WFname, onlyT1, master_config,
     sessionMakeMultimodalInput.inputs.maxIslandCount = 5
     sessionMakeMultimodalInput.inputs.useFullyConnected = True
     sessionMakeMultimodalInput.inputs.forceLabelChange = True
+    sessionMakeMultimodalInput.inputs.noDilation = False
     sessionMakeMultimodalInput.inputs.includeList = None
     sessionMakeMultimodalInput.inputs.excludeList = '4,5,14,15,21,24,31,43,44,63,72,85,98,128,219,15000'
 

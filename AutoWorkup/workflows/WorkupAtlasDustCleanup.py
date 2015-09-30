b31f8c780778c929d41cdd7a4e9f62f93852d1c2
Jessica Forbes <jessica-forbes@uiowa.edu>
2015-09-30 12:03:16 -0500

    ENH: Adds suspicious dust cleaning node before other cleaning node

--- AutoWorkup/workflows/WorkupAtlasDustCleanup.py
+++ AutoWorkup/workflows/WorkupAtlasDustCleanup.py
@@ -3,8 +3,8 @@
 from nipype.interfaces.utility import Function, IdentityInterface
 import nipype.pipeline.engine as pe  # pypeline engine
 
-def MakeVector(inFN1, inAtlas, outAtlas, maxIslandCount,
-               useFullyConnected, forceLabelChange,
+def runAutomaticCleanupScript(inFN1, inAtlas, outAtlas, maxIslandCount,
+               useFullyConnected, forceLabelChange, noDilation,
                inFN2=None, includeList=None, excludeList=None):
     arguments = {'--inputT1Path': inFN1,
                  '--inputT2Path': inFN2,
@@ -13,17 +13,16 @@ def MakeVector(inFN1, inAtlas, outAtlas, maxIslandCount,
                  '--maximumIslandVoxelCount': maxIslandCount,
                  '--useFullyConnectedInConnectedComponentFilter': useFullyConnected,
                  '--forceSuspiciousLabelChange': forceLabelChange,
+                 '--noDilation': noDilation,
                  '--includeLabelsList': includeList,
                  '--excludeLabelsList': excludeList
                }
-    return arguments
 
-def runAutomaticCleanupScript(arguments):
-  from atlasSmallIslandCleanup import DustCleanup
-  print arguments
-  localDustCleanupObject = DustCleanup(arguments=arguments)
-  localDustCleanupObject.main()
-  return arguments['--outputAtlasPath']
+    from atlasSmallIslandCleanup import DustCleanup
+    print arguments
+    localDustCleanupObject = DustCleanup(arguments=arguments)
+    localDustCleanupObject.main()
+    return outAtlas
 
 def CreateDustCleanupWorkflow(WFname, onlyT1, master_config,
                               runFixFusionLabelMap=True):
@@ -49,38 +48,62 @@ def CreateDustCleanupWorkflow(WFname, onlyT1, master_config,
                           name='outputspec')
 
     """
-    multimodal atlas dust cleanup if t2 exists
+    Multimodal atlas dust cleanup if T2 exists to clean 'suspicious' dust. This stage builds islands
+    using four-neighbor connectivity (useFullyConnected = False), has a max island count of 6 (instead
+    of 5 as in the next stage), forces labels to change for these islands, does NOT dilate the label
+    mask in order to clean all dust particles even clusters, and only suspicious (999) is cleaned.
     """
-    sessionMakeMultimodalInput = pe.Node(Function(function=MakeVector,
+    sessionRunDustCleanupOnSuspicious = pe.Node(Function(function=runAutomaticCleanupScript,
                                                   input_names=['inFN1', 'inFN2', 'inAtlas', 'outAtlas',
                                                                'maxIslandCount', 'useFullyConnected',
                                                                'forceLabelChange', 'noDilation',
                                                                'includeList', 'excludeList'],
-                                                  output_names=['arguments']),
-                                run_without_submitting=True, name="sessionMakeMultimodalInput")
-    dustCleanupWF.connect(inputsSpec, 'subj_t1_image', sessionMakeMultimodalInput, 'inFN1')
+                                                  output_names=['cleanedLabelImage']),
+                                run_without_submitting=True, name="sessionRunDustCleanupOnSuspicious")
+    dustCleanupWF.connect(inputsSpec, 'subj_t1_image', sessionRunDustCleanupOnSuspicious, 'inFN1')
     if not onlyT1:
-        dustCleanupWF.connect(inputsSpec, 'subj_t2_image', sessionMakeMultimodalInput, 'inFN2')
+        dustCleanupWF.connect(inputsSpec, 'subj_t2_image', sessionRunDustCleanupOnSuspicious, 'inFN2')
     else:
         pass
 
-    dustCleanupWF.connect(inputsSpec, 'subj_label_atlas', sessionMakeMultimodalInput, 'inAtlas')
-    sessionMakeMultimodalInput.inputs.outAtlas = 'dustCleanedLabel_maxIsland5.nii.gz'
-    sessionMakeMultimodalInput.inputs.maxIslandCount = 5
-    sessionMakeMultimodalInput.inputs.useFullyConnected = True
-    sessionMakeMultimodalInput.inputs.forceLabelChange = True
-    sessionMakeMultimodalInput.inputs.noDilation = False
-    sessionMakeMultimodalInput.inputs.includeList = None
-    sessionMakeMultimodalInput.inputs.excludeList = '4,5,14,15,21,24,31,43,44,63,72,85,98,128,219,15000'
+    dustCleanupWF.connect(inputsSpec, 'subj_label_atlas', sessionRunDustCleanupOnSuspicious, 'inAtlas')
+    sessionRunDustCleanupOnSuspicious.inputs.outAtlas = 'dustCleanedLabel_Suspicious_maxIsland6.nii.gz'
+    sessionRunDustCleanupOnSuspicious.inputs.maxIslandCount = 6
+    sessionRunDustCleanupOnSuspicious.inputs.useFullyConnected = False
+    sessionRunDustCleanupOnSuspicious.inputs.forceLabelChange = True
+    sessionRunDustCleanupOnSuspicious.inputs.noDilation = True
+    sessionRunDustCleanupOnSuspicious.inputs.includeList = 999
+    sessionRunDustCleanupOnSuspicious.inputs.excludeList = None
 
-
-    sessionRunDustCleanupInput = pe.Node(Function(function=runAutomaticCleanupScript,
-                                                  input_names=['arguments'],
+    """
+    Multimodal atlas dust cleanup if T2 exists to clean most labels after suspicious has been cleaned.
+    Labels excluded from this cleaning stage may have viable isolated islands that should not be changed.
+    This stage builds islands using eight-neighbor connectivity (useFullyConnected = True), has a max
+    island count of 5 (instead of 6 as in the previous stage), forces labels to change for these islands,
+    does dilate the label mask to avoid cleaning clustered dust, and several labels are excluded.
+    """
+    sessionRunDustCleanup = pe.Node(Function(function=runAutomaticCleanupScript,
+                                                  input_names=['inFN1', 'inFN2', 'inAtlas', 'outAtlas',
+                                                               'maxIslandCount', 'useFullyConnected',
+                                                               'forceLabelChange', 'noDilation',
+                                                               'includeList', 'excludeList'],
                                                   output_names=['cleanedLabelImage']),
-                                run_without_submitting=True, name="sessionRunDustCleanupInput")
+                                run_without_submitting=True, name="sessionRunDustCleanup")
+    dustCleanupWF.connect(inputsSpec, 'subj_t1_image', sessionRunDustCleanup, 'inFN1')
+    if not onlyT1:
+        dustCleanupWF.connect(inputsSpec, 'subj_t2_image', sessionRunDustCleanup, 'inFN2')
+    else:
+        pass
 
-    dustCleanupWF.connect(sessionMakeMultimodalInput, 'arguments', sessionRunDustCleanupInput, 'arguments')
+    dustCleanupWF.connect(sessionRunDustCleanupOnSuspicious, 'cleanedLabelImage', sessionRunDustCleanup, 'inAtlas')
+    sessionRunDustCleanup.inputs.outAtlas = 'dustCleanedLabel_maxIsland5.nii.gz'
+    sessionRunDustCleanup.inputs.maxIslandCount = 5
+    sessionRunDustCleanup.inputs.useFullyConnected = True
+    sessionRunDustCleanup.inputs.forceLabelChange = True
+    sessionRunDustCleanup.inputs.noDilation = False
+    sessionRunDustCleanup.inputs.includeList = None
+    sessionRunDustCleanup.inputs.excludeList = '4,5,14,15,21,24,31,43,44,63,72,85,98,128,219,15000'
 
-    dustCleanupWF.connect(sessionRunDustCleanupInput, 'cleanedLabelImage', outputsSpec, 'dustCleanedOutputAtlas_label')
+    dustCleanupWF.connect(sessionRunDustCleanup, 'cleanedLabelImage', outputsSpec, 'dustCleanedOutputAtlas_label')
 
     return dustCleanupWF

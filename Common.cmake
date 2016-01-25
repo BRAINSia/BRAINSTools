f82a99e9e974157bea10b1d5d867dbb14eacbabf
Hans Johnson <hans-johnson@uiowa.edu>
2016-01-25 07:47:37 -0600

    STYLE: Remove MinSizeRel option
    This is removed mostly for testing purposes. It
    only affects cmake gui options.

--- Common.cmake
+++ Common.cmake
@@ -34,8 +34,10 @@ endif()
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to 'Release' as none was specified.")
   set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
+  set(CTEST_CONFIGURATION_TYPE ${CMAKE_BUILD_TYPE} CACHE STRING "Choose the type of test." FORCE)
   # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo")
+  #  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
 
 if(${ITK_VERSION_MAJOR} STREQUAL "3")

8693f88dc606db3f4f654afdb003f46c6ed43941
Hans Johnson <hans-johnson@uiowa.edu>
2016-04-17 13:40:24 +0200

    STYLE: Consolidate common typedef in one location
    Applying the principle that a concept shall be defined
    in only one place, and that it should be consistently
    defined.

---
+++ BRAINSCommonLib/BRAINSTypes.h
@@ -0,0 +1,8 @@
+#ifndef BRAINSTypes_H__
+#define BRAINSTypes_H__
+
+#include "itkImageMaskSpatialObject.h"
+typedef itk::SpatialObject<3>      SpatialObjectType;
+typedef SpatialObjectType::Pointer ImageMaskPointer;
+
+#endif //BRAINSTypes_H__

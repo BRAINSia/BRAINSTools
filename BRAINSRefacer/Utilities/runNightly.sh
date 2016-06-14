e051221179d78fd7da25bd199e2a51d9464cb251
Alexander Leinoff <alexander-leinoff@uiowa.edu>
2016-06-14 13:21:10 -0500

    ENH: Adds unit test parameters for BrainDataTest in module BRAINSRefacer
    Before and after images are added to the BrainDataTest to see if our
    program altered any of the brain region while refacing.
    
    ENH: Adds support for nightly dashboard submission in module BRAINSRefacer
    
    Added a bash script to automatically submit to the dashboard.
    The bash script paths should be edited to suit your environment.
    This dashboard will be removed when BRAINSRefacer branch is merged into master
    
    ENH: Adds some more basic run tests
    
    Our program fails to run properly with some types of files. The culprit is
    probably spacing, or dfferent image sizes. The new tests run our main program
    with different files as input, and some fail. If the tests pass, then we know
    that at least some problems have been fixed.

---
+++ BRAINSRefacer/Utilities/runNightly.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Script to run the Test Suite for BRAINSRefacer nightly
+# Change the paths of the directories below
+# This needs to be copied to and run from a location outside of the source directory.
+
+BASE_DIR=~/code/brains/alexProject
+SOURCE_DIR=${BASE_DIR}/BRAINSTools
+BUILD_DIR=${BASE_DIR}/nightlyBuild
+EXTERNAL_SOURCES_DIR=~/code/brains/nightly/build
+
+
+if [[ ! -d ${BASE_DIR} ]]; then
+  echo "base directory doesn't exist";
+  exit -1;
+fi
+
+echo "updating source repo"
+if [[ ! -d ${SOURCE_DIR} ]]; then
+  echo "source directory doesn't exist. Cloning.";
+  git clone http://github.com/BRAINSIa/BRAINSTools.git;
+  cd ${SOURCE_DIR}
+  git checkout BRAINSRefacer
+else
+  cd ${SOURCE_DIR};
+  git fetch origin;
+  git checkout master;
+  git branch -D BRAINSRefacer
+  git checkout BRAINSRefacer
+fi
+
+echo "changing to build directory"
+if [[ ! -d ${BUILD_DIR} ]]; then
+  echo "build directory doesnt exist. Creating ..."
+  mkdir -p ${BUILD_DIR};
+fi
+
+cd ${BUILD_DIR};
+
+echo "Configuring with cmake"
+/usr/local/bin/cmake ${SOURCE_DIR} -DBRAINSTools_SUPERBUILD:BOOL=OFF -DSlicerExecutionModel_DIR:PATH=${EXTERNAL_SOURCES_DIR}/SlicerExecutionModel-build -DCMAKE_MODULE_PATH:PATH=${SOURCE_DIR}/CMake -DUSE_BRAINSRefacer=ON -DUSE_ANTS=OFF -DUSE_AutoWorkup=OFF -DUSE_BRAINSABC=OFF -DUSE_BRAINSConstellationDetector=OFF -DUSE_BRAINSDWICleanup=OFF -DUSE_BRAINSFit=OFF -DUSE_BRAINSInitializedControlPoints=OFF -DUSE_BRAINSLabelStats=OFF -DUSE_BRAINSLandmarkInitializer=OFF -DUSE_BRAINSROIAuto=OFF -DUSE_BRAINSResample=OFF -DUSE_BRAINSSnapShotWriter=OFF -DUSE_BRAINSStripRotation=OFF -DUSE_BRAINSTransformConvert=OFF -DUSE_DWIConvert=OFF -DUSE_ConvertBetweenFileFormats=OFF -DUSE_ImageCalculator=OFF -DUSE_ReferenceAtlas=OFF
+
+echo "Building";
+make -j24 -k;
+make -j24 -k;
+
+echo "Testing"
+/usr/local/bin/ctest -j12 -D NightlyStart;
+/usr/local/bin/ctest -j12 -D NightlyConfigure;
+/usr/local/bin/ctest -j12 -D NightlyBuild;
+/usr/local/bin/ctest -j12 -D NightlyTest;
+/usr/local/bin/ctest -j12 -D NightlySubmit;

BRAINSia/BRAINSTools

commit e808fbeaaf6fe4f15e23a756472b75af7ff17353

ENH: Remove shadowed typedefs
style: variableHidingTypedef: The typedef hides a typedef with the same name.

BRAINSDemonWarp/BRAINSDemonWarpTemplates.h
@@ -179,8 +179,7 @@ void ThirionFunction(const struct BRAINSDemonWarpAppParameters & command)
   typedef itk::Image<MaskPixelType, dims>                 MaskImageType;
   typedef itk::CastImageFilter<TRealImage, MaskImageType> CastImageFilter;
 
-  typedef itk::ImageMaskSpatialObject<dims>                           ImageMaskSpatialObjectType;
-  typedef itk::SpatialObject<3> SpatialObjectType;
+  typedef itk::ImageMaskSpatialObject<dims>               ImageMaskSpatialObjectType;
 
   //
   // If optional landmark files given, will use landmark registration to

BRAINSCommonLib/BRAINSFitHelperTemplate.h
@@ -276,7 +276,7 @@ protected:
   /** instantiate and call the Registration Helper */
   template <class TransformType,
             class OptimizerType,
-            class MetricType>
+            class FitCommonCodeMetricType>
   void FitCommonCode(int numberOfIterations,
                      double minimumStepLength,
                      typename CompositeTransformType::Pointer & initialITKTransform);

BRAINSCommonLib/BRAINSFitHelperTemplate.hxx
@@ -139,7 +139,7 @@ ValidateTransformRankOrdering(const std::vector<std::string> & transformType)
 }
 
 template <class FixedImageType, class MovingImageType, class TransformType,
-          class SpecificInitializerType, typename MetricType>
+          class SpecificInitializerType, typename DoCenteredInitializationMetricType>
 typename TransformType::Pointer
 DoCenteredInitialization( typename FixedImageType::Pointer & orientedFixedVolume,
                           typename MovingImageType::Pointer & orientedMovingVolume,
@@ -150,7 +150,7 @@ DoCenteredInitialization( typename FixedImageType::Pointer & orientedFixedVolume
                                                                     // variable,  the Mask is updated by
                                                                     // this function
                           std::string & initializeTransformMode,
-                          typename MetricType::Pointer & CostMetricObject )
+                          typename DoCenteredInitializationMetricType::Pointer & CostMetricObject )
 {
   typedef itk::Image<unsigned char, 3>                               MaskImageType;
   typedef itk::ImageMaskSpatialObject<MaskImageType::ImageDimension> ImageMaskSpatialObjectType;
@@ -575,7 +575,7 @@ BRAINSFitHelperTemplate<FixedImageType, MovingImageType>::BRAINSFitHelperTemplat
 }
 
 template <class FixedImageType, class MovingImageType>
-template <class TransformType, class OptimizerType, class MetricType>
+template <class TransformType, class OptimizerType, class FitCommonCodeMetricType>
 void
 BRAINSFitHelperTemplate<FixedImageType, MovingImageType>::FitCommonCode(
   int numberOfIterations,
@@ -588,7 +588,7 @@ BRAINSFitHelperTemplate<FixedImageType, MovingImageType>::FitCommonCode(
      OptimizerType,
      FixedImageType,
      MovingImageType,
-     MetricType> MultiModal3DMutualRegistrationHelperType;
+     FitCommonCodeMetricType> MultiModal3DMutualRegistrationHelperType;
 
   typename MultiModal3DMutualRegistrationHelperType::Pointer
   appMutualRegistration = MultiModal3DMutualRegistrationHelperType::New();

BRAINSCommonLib/BRAINSTypes.h
@@ -2,7 +2,15 @@
 #define BRAINSTypes_H__
 
 #include "itkImageMaskSpatialObject.h"
+#include <map>
+#include <string>
+
 typedef itk::SpatialObject<3>      SpatialObjectType;
 typedef SpatialObjectType::Pointer ImageMaskPointer;
+
+typedef itk::Point<double, 3>                    LandmarkPointType;
+typedef std::map<std::string, LandmarkPointType> LandmarksMapType;
+typedef std::map<std::string, float>             LandmarkWeightMapType;
+typedef std::map<std::string, double>            LandmarksWeightMapType;
 
 #endif //BRAINSTypes_H__

BRAINSCommonLib/Slicer3LandmarkIO.h
@@ -32,15 +32,13 @@
 #include <map>
 #include <string>
 
+#include <BRAINSTypes.h>
+
 /*
  * This IO utility program transforms between ITK landmarks (in LPS coordinate
  * system) to Slicer3 landmarks (in RAS coordinate system).
  */
 
-typedef itk::Point<double, 3>                    LandmarkPointType;
-typedef std::map<std::string, LandmarkPointType> LandmarksMapType;
-typedef std::map<std::string, float>             LandmarkWeightMapType;
-typedef std::map<std::string, double>            LandmarksWeightMapType;
 
 /*
  * Read lmk weights

BRAINSCommonLib/Slicer3LandmarkWeightIO.h
@@ -32,12 +32,12 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <BRAINSTypes.h>
 
 /*
  * This IO utility program write and read the ITK landmarks weight.
  */
 
-typedef std::map<std::string, double> LandmarksWeightMapType;
 
 /*
  * Write ITK landmark weights to a Slicer3 landmark list file (.wts)

BRAINSResample/BRAINSResample.cxx
@@ -145,7 +145,6 @@ int main(int argc, char *argv[])
     // An empty SmartPointer constructor sets up someImage.IsNull() to represent a not-supplied state:
     TBRAINSResampleReferenceImageType::Pointer ReferenceImage;
 
-    typedef itk::ImageFileReader<TBRAINSResampleReferenceImageType> ReaderType;
     ReaderType::Pointer refImageReader = ReaderType::New();
     if( referenceVolume.size() > 0 )
       {

commit 6763cdb163bb697d27a8603082851d39f7e27781

COMP: BRAINSDemonWarpTemplates: Fix unknown SpatialObjectType error.
Fixes #285

This commit fixes a regression introduced in e808fbe (ENH: Remove
shadowed typedefs). It addresses errors like these ones:

In file included from /path/to/BRAINSTools/BRAINSDemonWarp/BRAINSDemonWarpTemplates_Vdouble.cxx:19:0:
/path/to/BRAINSTools/BRAINSDemonWarp/BRAINSDemonWarpTemplates.h: In function ‘void ThirionFunction(const BRAINSDemonWarpAppParameters&)’:
/path/to/BRAINSTools/BRAINSDemonWarp/BRAINSDemonWarpTemplates.h:312:55: error: ‘SpatialObjectType’ does not name a type
         actualfilter->SetFixedImageMask( dynamic_cast<SpatialObjectType *>( fixedMask.GetPointer() ) );
                                                       ^
/path/to/BRAINSTools/BRAINSDemonWarp/BRAINSDemonWarpTemplates.h:312:73: error: expected ‘>’ before ‘*’ token
         actualfilter->SetFixedImageMask( dynamic_cast<SpatialObjectType *>( fixedMask.GetPointer() ) );
                                                                         ^

BRAINSDemonWarp/BRAINSDemonWarpTemplates.h
@@ -39,6 +39,7 @@
 #include "itkArray.h"
 
 #include "BRAINSCommonLib.h"
+#include "BRAINSTypes.h"
 
 #include "BRAINSDemonWarpCommonLibWin32Header.h"
 #include "GenericTransformImage.h"
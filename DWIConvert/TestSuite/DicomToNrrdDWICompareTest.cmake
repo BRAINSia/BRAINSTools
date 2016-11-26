373b7919911f5442c2ed2b2740d3e9e6e4d76eba
Hans Johnson <hans-johnson@uiowa.edu>
2016-11-26 16:10:52 -0600

    ENH: Add extra testing for FSL conversions.

--- DWIConvert/TestSuite/DicomToNrrdDWICompareTest.cmake
+++ DWIConvert/TestSuite/DicomToNrrdDWICompareTest.cmake
@@ -32,22 +32,83 @@ if(NOT EXISTS ${TEST_BASELINE})
   message( FATAL_ERROR "Failed: Baseline image ${TEST_BASELINE} does not exist!\n")
 endif( NOT EXISTS ${TEST_BASELINE})
 
+
 # run the test program, capture the stdout/stderr and the result var
-set(Test_Command_Line
-  ${TEST_PROGRAM} --inputDicomDirectory ${TEST_INPUT} --outputVolume ${TEST_TEMP_OUTPUT} ${TEST_PROGRAM_ARGS}
-  )
 if(TEST_FSL_FLAG)
+  set(Test_Command_Line
+    ${TEST_PROGRAM} --inputDicomDirectory ${TEST_INPUT} --outputVolume ${TEST_TEMP_OUTPUT} ${TEST_PROGRAM_ARGS}
+  )
   list(INSERT Test_Command_Line 1 --conversionMode)
   list(INSERT Test_Command_Line 2 DicomToFSL)
 
-endif()
-
-message("Test_Command_Line=${Test_Command_Line}")
-execute_process(
-  COMMAND ${Test_Command_Line}
-  ERROR_VARIABLE TEST_ERROR
-  RESULT_VARIABLE TEST_RESULT
+  message("Test_Command_Line=${Test_Command_Line}")
+  execute_process(
+    COMMAND ${Test_Command_Line}
+    ERROR_VARIABLE TEST_ERROR
+    RESULT_VARIABLE TEST_RESULT
   )
+  if( TEST_RESULT )
+    message( FATAL_ERROR "Failed: Test program ${TEST_PROGRAM} exited != 0.\n${TEST_ERROR}" )
+  endif( TEST_RESULT )
+
+else()
+  ## For testing purposes, convert from dicom to nhdr, nhdr to fsl, fsl to nrrd, then compare results
+  get_filename_component(TEST_TEMP_DIRNAME ${TEST_TEMP_OUTPUT} DIRECTORY)
+  get_filename_component(TEST_TEMP_BASENAME ${TEST_TEMP_OUTPUT} NAME_WE )
+  set(TEST_TEMP_FULLBASENAME "${TEST_TEMP_DIRNAME}/${TEST_TEMP_BASENAME}")
+
+
+  set(TEST_INITIAL_FSL_OUTPUT ${TEST_TEMP_FULLBASENAME}_NRRDTOFSL.nii.gz)
+  set(TEST_INITIAL_NHDR_OUTPUT ${TEST_TEMP_FULLBASENAME}_FSLTONHDR.nhdr)
+  file(REMOVE ${TEST_INITIAL_NHDR_OUTPUT})
+  file(REMOVE ${TEST_INITIAL_FSL_OUTPUT})
+  file(REMOVE ${TEST_TEMP_OUTPUT})
+
+
+  set(Test_Command_Line_DICOMToNHDR
+    ${TEST_PROGRAM} --conversionMode DicomToNrrd --inputDicomDirectory ${TEST_INPUT} --outputVolume ${TEST_TEMP_OUTPUT} ${TEST_PROGRAM_ARGS}
+  )
+  set(Test_Command_Line_NHDRToFSL
+    ${TEST_PROGRAM} --conversionMode NrrdToFSL --inputVolume ${TEST_TEMP_OUTPUT} --outputVolume ${TEST_INITIAL_FSL_OUTPUT} ${TEST_PROGRAM_ARGS}
+  )
+  set(Test_Command_Line_FSLToNrrd
+    ${TEST_PROGRAM} --conversionMode FSLToNrrd --inputVolume ${TEST_INITIAL_FSL_OUTPUT} --outputVolume ${TEST_INITIAL_NHDR_OUTPUT} ${TEST_PROGRAM_ARGS}
+  )
+
+  message(STATUS "\n\nTest_Command_Line_DICOMToNHDR=${Test_Command_Line_DICOMToNHDR}\n")
+  execute_process(
+    COMMAND ${Test_Command_Line_DICOMToNHDR}
+    ERROR_VARIABLE TEST_ERROR
+    RESULT_VARIABLE TEST_RESULT
+  )
+  message(STATUS ${TEST_ERROR})
+  if( TEST_RESULT )
+    message( FATAL_ERROR "Failed: Test program ${Test_Command_Line_DICOMToNHDR} exited != 0.\n${TEST_ERROR}" )
+  endif( TEST_RESULT )
+
+  message(STATUS "\n\nTest_Command_Line_NHDRToFSL=${Test_Command_Line_NHDRToFSL}\n")
+  execute_process(
+    COMMAND ${Test_Command_Line_NHDRToFSL}
+    ERROR_VARIABLE TEST_ERROR
+    RESULT_VARIABLE TEST_RESULT
+  )
+  message(STATUS ${TEST_ERROR})
+  if( TEST_RESULT )
+    message( FATAL_ERROR "Failed: Test program ${Test_Command_Line_NHDRToFSL} exited != 0.\n${TEST_ERROR}" )
+  endif( TEST_RESULT )
+
+  message(STATUS "\n\nTest_Command_Line_FSLToNrrd=${Test_Command_Line_FSLToNrrd}\n")
+  execute_process(
+    COMMAND ${Test_Command_Line_FSLToNrrd}
+    ERROR_VARIABLE TEST_ERROR
+    RESULT_VARIABLE TEST_RESULT
+  )
+  message(STATUS ${TEST_ERROR})
+  if( TEST_RESULT )
+    message( FATAL_ERROR "Failed: Test program ${Test_Command_Line_FSLToNrrd} exited != 0.\n${TEST_ERROR}" )
+  endif( TEST_RESULT )
+
+endif()
 
 if(TEST_FSL_FLAG)
   if(NOT TEST_BASELINE_BVEC)
@@ -105,11 +166,12 @@ execute_process(
   COMMAND ${Test_Compare_Command_Line}
   RESULT_VARIABLE TEST_RESULT
   )
-
 # again, if return value is !=0 scream and shout
 if( TEST_RESULT )
   message( FATAL_ERROR "Failed: The output of ${TEST_PROGRAM} did not match ${TEST_BASELINE}: ${TEST_RESULT}")
 endif( TEST_RESULT )
+
+
 
 # everything went fine...
 message( "Passed: The output of ${TEST_PROGRAM} matches ${TEST_BASELINE}" )

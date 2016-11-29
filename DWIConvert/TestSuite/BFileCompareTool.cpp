bee4302313cb31911361a4570423d0b007d689d1
Hans Johnson <hans-johnson@uiowa.edu>
2016-11-28 19:13:55 -0600

    ENH: Update command line tool for comparing BVec and BVal files.

--- DWIConvert/TestSuite/BFileCompareTool.cpp
+++ DWIConvert/TestSuite/BFileCompareTool.cpp
@@ -1,27 +1,30 @@
 #include "stdio.h"
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <cmath>
 
 typedef std::vector< std::vector< double > > DataVector;
+
+static const int USAGE_FAILURE_CODE=1;
+static const int FILE_READ_FAILURE_CODE=2;
+static const int VALUE_TOLERANCE_FAILURE_CODE=4;
 
 int readFileIntoBuffer(const char* filename, char** buffer){
   FILE* pFile = fopen(filename, "rb");
   if (NULL == pFile) {
     std::cerr<<"Sorry, can not open file: "<<filename<<std::endl;
-    return 2;
+    return FILE_READ_FAILURE_CODE;
   }
 
   fseek(pFile,0,SEEK_END);
-  int nFileSize = ftell(pFile);
+  long nFileSize = ftell(pFile);
   fseek(pFile,0,SEEK_SET);
 
   *buffer = new char[nFileSize+1];
-  int readSize = fread(*buffer, sizeof(char), nFileSize, pFile);
+  long readSize = fread(*buffer, sizeof(char), (std::size_t) nFileSize, pFile);
   fclose(pFile);
   *(*buffer + readSize) = '\0';
-  return 0; //success
+  return EXIT_SUCCESS; //success
 }
 
 char* nextNoneSpacePointer(char* p){
@@ -31,7 +34,7 @@ char* nextNoneSpacePointer(char* p){
   return p;
 }
 
-//notes: afer using buffer, this function deletes buffer.
+//notes: after using buffer, this function deletes buffer.
 DataVector convertBufferIntoVector(char* buffer){
   DataVector ColumnData;
   char*p = nextNoneSpacePointer(buffer);
@@ -39,7 +42,7 @@ DataVector convertBufferIntoVector(char* buffer){
     std::vector< double > lineData;
     lineData.reserve(200);
     while ('\n' != *p && '\0' != *p){
-      int dataOffset = 0;
+      std::size_t dataOffset = 0;
       char *dataBeginning = p;
       while (' ' != *p && '\n' != *p && '\0' != *p) {
         ++dataOffset;
@@ -61,60 +64,60 @@ DataVector convertBufferIntoVector(char* buffer){
 }
 
 //return: 0: all comparing data are close to each other
-int compareRowColumn(const DataVector& data1, const DataVector& data2, const int linesToCompare, const double tolerance){
+int compareRowColumn(const DataVector& data1, const DataVector& data2, const size_t linesToCompare, const double tolerance){
   if (data1.size() != data2.size() && linesToCompare > std::min(data1.size(),data2.size())){
-     std::cout<< "two files have different numbers of lines needing comparing." <<std::endl;
-     return 4;
+    std::cout<< "two files have different numbers of lines needing comparing." <<std::endl;
+    return VALUE_TOLERANCE_FAILURE_CODE;
   }
 
   if ( linesToCompare > std::min(data1.size(),data2.size()) ) {
     std::cout<< "linesToCompare is greater than the number of data lines." <<std::endl;
-    return 4;
+    return VALUE_TOLERANCE_FAILURE_CODE;
   }
 
-  for(int i=0; i< linesToCompare; ++i){
+  for(size_t i=0; i< linesToCompare; ++i){
     std::vector< double > row1 = data1[i];
     std::vector< double > row2 = data2[i];
-    int const column = row1.size();
+    const size_t column = row1.size();
     if (column != row2.size()){
-      printf("In the line %d, 2 data has different column.\n", i);
-      return 4;
+      printf("In the line %zu, 2 data has different column.\n", i);
+      return VALUE_TOLERANCE_FAILURE_CODE;
     }
-    for (int j=0; j< column; ++j){
-      if (fabs(row1[j] - row2[j]) > tolerance){
-        printf("In the line %d, column %d:\ndata1 has value: %.8f;\ndata2 has value: %.8f.\n", i+1,j+1,row1[j],row2[j]);
+    for (size_t j=0; j< column; ++j){
+      if (std::abs(row1[j] - row2[j]) > tolerance){
+        printf("In the line %zu, column %zu:\ndata1 has value: %.8f;\ndata2 has value: %.8f.\n", i+1,j+1,row1[j],row2[j]);
         printf("The difference is greater than the tolerance %g. \nSo data in two files are not close within specific tolerance.\nProgram exits.\n", tolerance);
-        return 4;
+        return VALUE_TOLERANCE_FAILURE_CODE;
       }
     }
   }
   printf("Two files are close within the specific tolerance %g \n", tolerance);
   printf("Program exits successfully.\n");
-  return 0;
+  return EXIT_SUCCESS;
 }
 
 
 int main(int argc, char * argv[])
 {
   if( argc != 5 )
-  {
+    {
     std::cerr << "USAGE: " << argv[0] << " <BFile1> <BFile2> <linesToCompare> <tolerance>" << std::endl;
     std::cerr << "Parameters have errors. Program exits." <<std::endl;
-    return 1;
-  }
+    return USAGE_FAILURE_CODE;
+    }
   const int linesToCompare = atoi(argv[3]);
   const double tolerance = atof(argv[4]);
 
 
   //Open files to read data into vector
   char* buffer1 = NULL;
-  char* buffer2 = NULL;
   if (0 != readFileIntoBuffer(argv[1], &buffer1)){
-    return 2;
+    return FILE_READ_FAILURE_CODE;
   }
+  char* buffer2 = NULL;
   if (0 != readFileIntoBuffer(argv[2], &buffer2)){
     delete[] buffer1;
-    return 2;
+    return FILE_READ_FAILURE_CODE;
   }
 
   DataVector data1 = convertBufferIntoVector(buffer1);

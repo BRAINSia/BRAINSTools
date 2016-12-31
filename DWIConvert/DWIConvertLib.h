325d9939ebb1ecfe6cd49890e56cf84615753bd8
Hui-Xie <Hui-Xie@uiowa.edu>
2016-12-30 18:44:53 -0600

    ENH: change data member into private

--- DWIConvert/DWIConvertLib.h
+++ DWIConvert/DWIConvertLib.h
@@ -11,22 +11,40 @@
 
 class DWIConvert {
 public:
-    //todo: get and set functions later
-    std::string m_inputVolume;
-    std::string m_inputDicomDirectory;
-    std::string m_inputBValues;  //default: ""  ??
-    std::string m_inputBVectors; //default: ""  ??
-    std::string m_gradientVectorFile; //deprecated
-    double m_smallGradientThreshold; //default = 0.2
+    DWIConvert();
+
+    std::string setConversionMode(); //according input params, automatically set conversion mode
+    void setConversionMode(const std::string conversionMode);
+
+    std::string getConversionMode();
+
+    //rewrite DWIConvert1, there is a test failure needing to debug
+    //Please use DWIConvert2 interface instead
+    int DWIConvert1();
+
+    DWIConverter *CreateDicomConverter(
+            const std::string inputDicomDirectory,
+            const bool useBMatrixGradientDirections,
+            const bool transpose,
+            const double smallGradientThreshold,
+            const bool allowLossyConversion);
+
+    //according Hans's code, encapsulate the DWIConvert2
+    int DWIConvert2();
+
+    int DWIConvertRead();
+
+    int DWIConvertWrite();
 
 
-    bool m_fMRIOutput; //default: false
-    bool m_transpose; //default:false
-    bool m_allowLossyConversion; //defualt: false
-    bool m_useIdentityMeasurementFrame; //default: false
-    bool m_useBMatrixGradientDirections; //default: false
 
-    std::string m_outputVolume;
+//application case use in gtractConcatDWI.cxx
+/*  in DWIConverter class:
+ *  const DWIMetaDataDictionaryValidator::GradientTableType &GetDiffusionVectors();
+ *  const std::vector<double> &GetBValues() const;
+ *  Volume3DUnwrappedType::PointType GetOrigin() const;
+ *  Volume3DUnwrappedType::Pointer getVolumePointer();
+ * */
 
     const std::string &getInputVolume() const;
 
@@ -88,50 +106,28 @@ public:
 
     void setOutputBVectors(const std::string &outputBVectors);
 
-    std::string m_outputDirectory;  //default: "."
-    std::string m_outputBValues; //default: ""
-    std::string m_outputBVectors;//default: ""
-
-    DWIConvert();
-
-    std::string setConversionMode(); //according input params, automatically set conversion mode
-    void setConversionMode(const std::string conversionMode);
-
-    std::string getConversionMode();
-
-    //rewrite DWIConvert1, there is a test failure needing to debug
-    //Please use DWIConvert2 interface instead
-    int DWIConvert1();
-
-    DWIConverter *CreateDicomConverter(
-            const std::string inputDicomDirectory,
-            const bool useBMatrixGradientDirections,
-            const bool transpose,
-            const double smallGradientThreshold,
-            const bool allowLossyConversion);
-
-    //according Hans's code, encapsulate the DWIConvert2
-    int DWIConvert2();
-
-    int DWIConvertRead();
-
-    int DWIConvertWrite();
-
-
-
-//application case use in gtractConcatDWI.cxx
-/*  in DWIConverter class:
- *  const DWIMetaDataDictionaryValidator::GradientTableType &GetDiffusionVectors();
- *  const std::vector<double> &GetBValues() const;
- *  Volume3DUnwrappedType::PointType GetOrigin() const;
- *  Volume3DUnwrappedType::Pointer getVolumePointer();
- * */
-
-
 private:
     std::string findFilenameExt(const std::string filename);
 
     std::string m_conversionMode;
+
+    std::string m_inputVolume;
+    std::string m_inputDicomDirectory;
+    std::string m_inputBValues;  //default: ""  ??
+    std::string m_inputBVectors; //default: ""  ??
+    std::string m_gradientVectorFile; //deprecated
+    double m_smallGradientThreshold; //default = 0.2
+
+    bool m_fMRIOutput; //default: false
+    bool m_transpose; //default:false
+    bool m_allowLossyConversion; //defualt: false
+    bool m_useIdentityMeasurementFrame; //default: false
+    bool m_useBMatrixGradientDirections; //default: false
+
+    std::string m_outputVolume;
+    std::string m_outputDirectory;  //default: "."
+    std::string m_outputBValues; //default: ""
+    std::string m_outputBVectors;//default: ""
 
 };
 

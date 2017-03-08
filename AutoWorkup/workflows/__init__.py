c6547b92eb9ada9ec92665d464b76692185f463a
Hans Johnson <hans-johnson@uiowa.edu>
2017-03-08 12:39:37 -0600

    ENH: remove unncessary import commands from __init__.py

--- AutoWorkup/workflows/__init__.py
+++ AutoWorkup/workflows/__init__.py
@@ -3,10 +3,11 @@ try:
 except ImportError:
     import AutoWorkup.utilities
 # import AutoWorkup.semtools
-import atlasNode
-import baseline
-import databaseNode
-import segmentation
-import singleSession
-import template
-import utils
+
+#import atlasNode
+#import baseline
+#import databaseNode
+#import segmentation
+#import singleSession
+#import template
+#import utils

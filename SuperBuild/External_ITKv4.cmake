cc37416ee49c8ec5c1879886fb0708fac93d1bdb
Hans Johnson <hans-johnson@uiowa.edu>
2017-05-15 09:37:15 -0500

    ENH: Update ITK to latest version.

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -40,7 +40,7 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   endif()
 
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  set(${proj}_GIT_TAG ef14cce1c26d5dce7eb2e10d36c7dc81aaa9c9e8 ) # 20170315
+  set(${proj}_GIT_TAG 93cee1ca38d60abaeeca422ac9712aa7c32a35db ) # 20170515
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 
   if(NOT ${CMAKE_PROJECT_NAME}ITKV3_COMPATIBILITY AND CMAKE_CL_64)

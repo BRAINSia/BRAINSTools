3db275fa49765b5a3234fe69cbbe8a1993155978
Hans Johnson <hans-johnson@uiowa.edu>
2017-05-16 17:28:10 -0500

    ENH: Fix VTK download URL.

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -99,10 +99,9 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
   if(NOT DEFINED git_protocol)
     set(git_protocol "git")
   endif()
-  # set(${proj}_GIT_REPOSITORY "${git_protocol}://github.com/Slicer/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
-  # set(${proj}_GIT_TAG "ea7cdc4e0b399be244e79392c67fed068c33e454")  # VTK 20141221
-  set(${proj}_GIT_REPOSITORY "${git_protocol}://gitlab.kitware.com/vtk/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
-  set(${proj}_GIT_TAG "da7bcf245ed84e7445bf5f6b66c55fe7811d6eff")  # VTK 20161215
+  set(vtk_git_protocol "https")
+  set(${proj}_GIT_REPOSITORY "${vtk_git_protocol}://gitlab.kitware.com/vtk/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
+  set(${proj}_GIT_TAG "1e4e7bd8822b25220586bb6ea0bd58a9ad2155f9")  # VTK 20170516
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}

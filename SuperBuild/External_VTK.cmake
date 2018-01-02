dbe6074312b4d1c56c0013ce6542a0c028faad13
Hans Johnson <hans-johnson@uiowa.edu>
2018-01-02 08:21:59 -0600

    ENH: Updated VTK with C++11 override instrumentation.

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -101,7 +101,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
   endif()
   set(vtk_git_protocol "https")
   set(${proj}_GIT_REPOSITORY "${vtk_git_protocol}://gitlab.kitware.com/vtk/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
-  set(${proj}_GIT_TAG "9712f4933b48dd503c186fda39d902cc39a3ecce")  # VTK 20171231
+  set(${proj}_GIT_TAG "61216dca2e7e7cc724b6ab12457fd9891b167bf0")  # VTK 20180102
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}

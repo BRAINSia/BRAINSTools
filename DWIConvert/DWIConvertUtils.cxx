BRAINSia/BRAINSTools

commit 147d810d50645f63555e497443e053dfc89c38b7

BUG: DWIConverter hard-coded transpose/FSLBy3Rows setting

DWIConvert/DWIConverter.cxx
@@ -209,7 +209,7 @@ void DWIConverter::ReadGradientInformation(const std::string& inputBValues, cons
   }
   DWIMetaDataDictionaryValidator::GradientTableType BVecs;
   unsigned int                      bVecCount = 0;
-  if( ReadBVecs(BVecs, bVecCount, _inputBVectors,true) != EXIT_SUCCESS )
+  if( ReadBVecs(BVecs, bVecCount, _inputBVectors, m_FSLFileFormatHorizontalBy3Rows) != EXIT_SUCCESS )
   {
     itkGenericExceptionMacro(<< "ERROR reading BVector " << _inputBVectors);
   }

commit faf6cdf47418302cff3e1215f315cbc016481bd7

ENH: Use file introspection to determine BVec file format
Prefer to inspect the BVec files directly rather than use a flag from
the function call.

Test were failing after 147d810d50645f63555e497443e053dfc89c38b7
and the NRRD files were quite different when round-trip conversion from
DICOM->FSL->NRRD or NRRD->FSL->NRRD conversions were requested.

Rather than depend on user input to select the format of the
BVec file, use introspection to determine the organization of
the files.

DWIConvert/DWIConvertUtils.cxx
@@ -158,20 +158,49 @@ ReadBVals(std::vector<double> & bVals, unsigned int & bValCount, const std::stri
   return EXIT_SUCCESS;
 }
 
-int
-ReadBVecs(DWIMetaDataDictionaryValidator::GradientTableType & bVecs, unsigned int & bVecCount, const std::string & bVecFilename , bool horizontalBy3Rows )
+static bool bVecFileIsHorizontalBy3Rows(const std::string & bVecFilename)
 {
   std::ifstream bVecFile(bVecFilename.c_str(), std::ifstream::in);
-
   if( !bVecFile.good() )
   {
     std::cerr << "Failed to open " << bVecFilename
               << std::endl;
     return EXIT_FAILURE;
   }
+  std::string line;
+  size_t num_lines = 0;
+  while( std::getline(bVecFile,line,'\n') )
+  {
+    if(line.size() > 5)
+    {
+      num_lines++;
+    }
+  }
+  if( num_lines > 5 ) // A minimum of 6 lines are needed for vertical format.
+  {
+    return false; // When more than 5 lines are found, assume vertical format
+  }
+  return true;  //When less than 5 lines are found, assume horizontal format (only 3 lines needed)
+}
+
+
+int
+ReadBVecs(DWIMetaDataDictionaryValidator::GradientTableType & bVecs, unsigned int & bVecCount, const std::string & bVecFilename )
+{
+
+  bool internal_bvec_organization_check = bVecFileIsHorizontalBy3Rows(bVecFilename);
+
+  std::ifstream bVecFile(bVecFilename.c_str(), std::ifstream::in);
+  if( !bVecFile.good() )
+  {
+    std::cerr << "Failed to open " << bVecFilename
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
   bVecs.clear();
   bVecCount = 0;
-  if( horizontalBy3Rows )
+  if( internal_bvec_organization_check )
   {
     std::vector<std::vector<double> > bVecst( 3 ) ;
     for( unsigned i = 0 ; i < 3 ; i++ )
@@ -220,7 +249,7 @@ ReadBVecs(DWIMetaDataDictionaryValidator::GradientTableType & bVecs, unsigned in
       bVecs.push_back(x);
     }
   }
-  else
+  else //BVECTOR FILE AS COLUMNS
   {
     while( !bVecFile.eof() )
     {

DWIConvert/DWIConvertUtils.h
@@ -122,7 +122,7 @@ extern int ReadBVals(std::vector<double> & bVals, unsigned int & bValCount,
                      const std::string & bValFilename);
 
 extern int ReadBVecs(DWIMetaDataDictionaryValidator::GradientTableType & bVecs, unsigned int & bVecCount,
-                     const std::string & bVecFilename , bool horizontalBy3Rows );
+                     const std::string & bVecFilename );
 
 template <typename TValue>
 bool

DWIConvert/DWIConverter.cxx
@@ -209,7 +209,7 @@ void DWIConverter::ReadGradientInformation(const std::string& inputBValues, cons
   }
   DWIMetaDataDictionaryValidator::GradientTableType BVecs;
   unsigned int                      bVecCount = 0;
-  if( ReadBVecs(BVecs, bVecCount, _inputBVectors, m_FSLFileFormatHorizontalBy3Rows) != EXIT_SUCCESS )
+  if( ReadBVecs(BVecs, bVecCount, _inputBVectors ) != EXIT_SUCCESS )
   {
     itkGenericExceptionMacro(<< "ERROR reading BVector " << _inputBVectors);
   }

DWIConvert/TestSuite/CMakeLists.txt
@@ -312,7 +312,7 @@ ExternalData_Add_Test( ${PROJECT_NAME}FetchData NAME DWIConvertPhilipsAchieva2BM
   -D TEST_COMPARE_PROGRAM=$<TARGET_FILE:DWICompare>
   -D TEST_BASELINE=DATA{${DWIBASELINE_DIR}/PhilipsAchieva2.nrrd}
   -D TEST_INPUT=${DWIConvert_BINARY_DIR}/PhilipsAchieva2
-  -D TEST_TEMP_OUTPUT=${TstOutput}/PhilipsAchieva2Test.nrrd
+  -D TEST_TEMP_OUTPUT=${TstOutput}/DWIConvertPhilipsAchieva2BMatrixTest.nrrd
   -D TEST_PROGRAM_ARGS=--useBMatrixGradientDirections
   -P ${CMAKE_CURRENT_LIST_DIR}/DicomToNrrdDWICompareTest.cmake
   )
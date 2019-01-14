adfc2503fb8f497485b651822b54cc1ba1044780
Hans Johnson <hans.j.johnson@gmail.com>
2019-01-14 09:29:46 -0600

    ENH: Fix failing CI .travis.yml syntax.

---
+++ .travis_build_script.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# -e: fail on error
+# -v: show commands
+# -x: show expanded commands
+set -vex
+
+
+SOURCE_DIR=$(pwd)
+BUILD_DIR=${SOURCE_DIR}/cmake-travis-bld
+
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+cmake \
+    -DCMAKE_CXX_STANDARD:STRING=11 \
+    -DUSE_ANTS=BOOL=OFF \
+    -DUSE_BRAINSABC:BOOL=OFF \
+    -DUSE_BRAINSDWICleanup:BOOL=OFF \
+    -DUSE_BRAINSInitializedControlPoints:BOOL=OFF \
+    -DUSE_BRAINSLabelStats:BOOL=OFF \
+    -DUSE_BRAINSLandmarkInitializer:BOOL=OFF \
+    -DUSE_BRAINSROIAuto:BOOL=OFF \
+    -DUSE_BRAINSRefacer:BOOL=OFF \
+    -DUSE_BRAINSResample:BOOL=OFF \
+    -DUSE_BRAINSSnapShotWriter:BOOL=OFF \
+    -DUSE_BRAINSStripRotation:BOOL=OFF \
+    -DUSE_BRAINSTransformConvert:BOOL=OFF \
+    -DUSE_ConvertBetweenFileFormats:BOOL=OFF \
+    -DUSE_ImageCalculator:BOOL=OFF \
+    -DUSE_ReferenceAtlas:BOOL=OFF \
+    ${SOURCE_DIR}
+
+make -j 2 # BUILD EVERYTHING: TODO: BUILD_SUPPORT_SEPARATE.
+
+cd BRAINSTools-build/
+make test

083ae59ed2067c6d817eacf90469290a24689a39
Hans Johnson <hans-johnson@uiowa.edu>
2019-01-17 21:22:51 -0600

    ENH: Wts file has header line to be ignored.
    The header line has a different format than the other lines.
    
    String,String
    String,Float

--- BRAINSCommonLib/Slicer3LandmarkIO.cxx
+++ BRAINSCommonLib/Slicer3LandmarkIO.cxx
@@ -26,6 +26,48 @@
 #include "itkNumberToString.h"
 #include "itkImageFileReaderException.h"
 
+//https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+    return !std::isspace(ch);
+  }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+    return !std::isspace(ch);
+  }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+  ltrim(s);
+  rtrim(s);
+}
+
+#if 0
+// trim from start (copying)
+static inline std::string ltrim_copy(std::string s) {
+  ltrim(s);
+  return s;
+}
+
+// trim from end (copying)
+static inline std::string rtrim_copy(std::string s) {
+  rtrim(s);
+  return s;
+}
+
+// trim from both ends (copying)
+static inline std::string trim_copy(std::string s) {
+  trim(s);
+  return s;
+}
+#endif
+
+
 LandmarkWeightMapType ReadLandmarkWeights( const std::string & weightFilename )
 {
   std::ifstream weightFileStream( weightFilename.c_str() );
@@ -40,13 +82,18 @@ LandmarkWeightMapType ReadLandmarkWeights( const std::string & weightFilename )
   LandmarkWeightMapType landmarkWeightMap;
   while( getline( weightFileStream, line ) )
     {
+    trim(line);
+
     const size_t      firstComma = line.find(',', 0);
     const std::string landmark = line.substr( 0, firstComma );
+    if ( line[0] == '#' || line.empty() || landmark == "Landmark" ) // For backwards compatibility, if the landmark name is "Landmark" then it is the header row.
+      {
+        continue;
+      }
     const float       weight   = std::stod( (line.substr( firstComma + 1, line.length() - 1 ) ).c_str() );
     landmarkWeightMap[landmark] = weight;
     }
-
-  return landmarkWeightMap;
+    return landmarkWeightMap;
 }
 
 

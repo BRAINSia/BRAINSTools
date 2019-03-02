708c4d0444c3655b2bbe9b39305d18725689702f
Hans Johnson <hans.j.johnson@gmail.com>
2019-03-02 10:10:12 -0600

    ENH: Added coments for travis building.

--- .travis_build_script.sh
+++ .travis_build_script.sh
@@ -15,6 +15,7 @@ cd ${BUILD_DIR}
 cmake \
     -DCMAKE_CXX_STANDARD:STRING=11 \
     -DUSE_ANTS=BOOL=OFF \
+    -DBRAINSTools_REQUIRES_VTK=OFF \
     -DUSE_BRAINSABC:BOOL=OFF \
     -DUSE_BRAINSDWICleanup:BOOL=OFF \
     -DUSE_BRAINSInitializedControlPoints:BOOL=OFF \
@@ -31,7 +32,8 @@ cmake \
     -DUSE_ReferenceAtlas:BOOL=OFF \
     ${SOURCE_DIR}
 
-make -j 2 # BUILD EVERYTHING: TODO: BUILD_SUPPORT_SEPARATE.
+#make -j 2 # BUILD EVERYTHING: TODO: BUILD_SUPPORT_SEPARATE.
+make TBB  && make
 
 cd BRAINSTools-build/
 make test

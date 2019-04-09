bf849651dacd3306e92806c0af219bd04ea0bdde
Hans Johnson <hans-johnson@uiowa.edu>
2019-04-09 07:25:29 -0500

    ENH: Update DCMTK version.

--- SuperBuild/External_DCMTK.cmake
+++ SuperBuild/External_DCMTK.cmake
@@ -66,7 +66,7 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     # plus the following patch:
     # * Set CMP0067 to ensure try_compile work as expected
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "982e037f314b7f64699f6b33215f65a0d19a5b55" # v3.6.3_20180621
+    "52cf5b48c113ff9c4100e10bba2bd45f21eeda62" # 20190331
     QUIET
     )
 

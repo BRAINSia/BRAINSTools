d9e734cc099d833ed202dc9b33e30837bfc710d5
Hans Johnson <hans.j.johnson@gmail.com>
2019-08-19 08:03:28 -0500

    COMP: Do not include itkExceptionMacro.h
    the itkExceptionMacro.h file must be included indirectly
    so that circular dependancies in the build are
    avoided.

--- BRAINSResample/BRAINSResize.cxx
+++ BRAINSResample/BRAINSResize.cxx
@@ -21,7 +21,6 @@
 #include <itkResampleImageFilter.h>
 #include <itkIdentityTransform.h>
 #include <itkLinearInterpolateImageFunction.h>
-#include <itkExceptionObject.h>
 #include "BRAINSResizeCLP.h"
 #include <BRAINSCommonLib.h>
 

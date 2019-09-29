98fbecffbeab82d409b5b41998ae85e6f76abb5d
Hans Johnson <hans.j.johnson@gmail.com>
2019-09-29 11:42:00 -0500

    ENH: Improve script robustness by quoting variables
    Variables should be quoted in shell scripts to avoid ambiguity in parsing tokens.
    Return of -1 is not valid, use 255 to indicate failure.
    
    Use python3 instead of python.
    Hide false warnings from tbb build where warnings are issued as diagnostic messages.

--- SuperBuild/build_tbb.sh
+++ SuperBuild/build_tbb.sh
@@ -27,7 +27,7 @@ done
 
 if [ -z ${BUILD_CACHE+x} ]; then
     echo "Usage was wrong, -b is a required option"
-    exit -1
+    exit 255
 fi
 if [ -z ${INSTALL_PREFIX+x} ]; then
     INSTALL_PREFIX=${BUILD_CACHE}/tbb-install
@@ -35,34 +35,37 @@ fi
 
 SRC_DIR=${BUILD_CACHE}
 
-SYSTEM_NAME=Darwin
+#SYSTEM_NAME=Darwin
 TBB_VERSION_FILE=${SRC_DIR}/include/tbb/tbb_stddef.h
 
-mkdir -p ${BUILD_CACHE}
-mkdir -p ${INSTALL_PREFIX}
+mkdir -p "${BUILD_CACHE}"
+mkdir -p "${INSTALL_PREFIX}"
 
-pushd ${BUILD_CACHE}
+pushd "${BUILD_CACHE}"
 
-python ${SRC_DIR}/build/build.py  \
-       --tbbroot ${SRC_DIR} \
-       --prefix ${INSTALL_PREFIX} \
-       --install-libs --install-devel --install-docs
+python3 "${SRC_DIR}/build/build.py"  \
+       --tbbroot "${SRC_DIR}" \
+       --prefix "${INSTALL_PREFIX}" \
+       --install-libs --install-devel --install-docs \
+  > /tmp/tbb_logger 2>&1
 
-cmake -DINSTALL_DIR=${INSTALL_PREFIX}/lib/cmake/tbb \
+#cat /tmp/tbb_logger
+
+cmake -DINSTALL_DIR="${INSTALL_PREFIX}/lib/cmake/tbb" \
       -DLIB_REL_PATH=../../../lib \
       -DINC_REL_PATH=../../../include \
-      -DSYSTEM_NAME=$(uname) \
-      -DTBB_VERSION_FILE=${TBB_VERSION_FILE} \
-      -P ${SRC_DIR}/cmake/tbb_config_installer.cmake
+      -DSYSTEM_NAME="$(uname)" \
+      -DTBB_VERSION_FILE="${TBB_VERSION_FILE}" \
+      -P "${SRC_DIR}/cmake/tbb_config_installer.cmake"
 
 
 DO_TESTING=1
 if [[ ${DO_TESTING} -eq 1 ]]; then
 
 TEST_DIR=${BUILD_CACHE}/tbb_test
-mkdir -p ${TEST_DIR}
+mkdir -p "${TEST_DIR}"
 
-cat > ${TEST_DIR}/CMakeLists.txt << EOF
+cat > "${TEST_DIR}/CMakeLists.txt" << EOF
 # Put this cmake file in a directory, run "cmake ."  finding TBB results in a FATAL_ERROR from cmake
 cmake_minimum_required(VERSION 3.0)
 # tbb;tbbmalloc;tbbmalloc_proxy
@@ -83,7 +86,7 @@ add_executable(tbb_test test.cpp)
 target_link_libraries(tbb_test TBB::tbb)
 EOF
 
-cat > ${TEST_DIR}/test.cpp << EOF
+cat > "${TEST_DIR}/test.cpp" << EOF
 #include <tbb/task_scheduler_init.h>
 #include <iostream>
 int main()
@@ -93,18 +96,19 @@ int main()
 }
 EOF
 
-mkdir -p ${TEST_DIR}-bld
-pushd ${TEST_DIR}-bld
-cmake -DCMAKE_BUILD_TYPE:STRING=Release -DTBB_DIR:PATH=${INSTALL_PREFIX}/lib/cmake/tbb  ${TEST_DIR}
+mkdir -p "${TEST_DIR}-bld"
+pushd "${TEST_DIR}-bld"
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -DTBB_DIR:PATH="${INSTALL_PREFIX}/lib/cmake/tbb"  "${TEST_DIR}"
 make
-./tbb_test
 
-if [[ $? -eq 0 ]]; then
+./tbb_test
+tbb_test_status=$?
+if [[ "${tbb_test_status}"  -eq 0 ]]; then
   echo "SUCCESSFUL TESTING"
   exit 0
 else
   echo "FAILED TESTING"
-  exit -1
+  exit 255
 fi
 
 fi

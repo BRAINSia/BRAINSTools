3d0cd9fb570573a86996e2ff5093edaa659ad64c
Hans Johnson <hans.j.johnson@gmail.com>
2020-01-09 10:08:44 -0600

    ENH: Update to ITKv5.1rc0

--- SuperBuild/External_ITKv5.cmake
+++ SuperBuild/External_ITKv5.cmake
@@ -40,7 +40,7 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   set(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITK.git)
   #set(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY ${git_protocol}://github.com/hjmjohnson/ITK.git)
-  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG e79b70f13afafa2eb0ef48f6ac90af185379080b ) #20191203 - ITKv5 Perf Enhancements
+  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG v5.1rc01 ) #20191207
 
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 

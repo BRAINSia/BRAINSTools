22adf54176014e16c58304890fd8982ecde3d82c
Hans Johnson <hans-johnson@uiowa.edu>
2020-03-13 17:44:01 -0500

    COMP: Fix implicit vnl conversion warnings.

--- BRAINSCommonLib/itkOrthogonalize3DRotationMatrix.cxx
+++ BRAINSCommonLib/itkOrthogonalize3DRotationMatrix.cxx
@@ -23,7 +23,7 @@ namespace itk
 itk::Matrix<double, 3, 3>
 Orthogonalize3DRotationMatrix(const itk::Matrix<double, 3, 3> & rotator)
 {
-  vnl_svd<double>                             decomposition(rotator.GetVnlMatrix(), -1e-6);
+  vnl_svd<double>                             decomposition(rotator.GetVnlMatrix().as_matrix(), -1e-6);
   vnl_diag_matrix<vnl_svd<double>::singval_t> Winverse(decomposition.Winverse());
 
   vnl_matrix<double> W(3, 3);

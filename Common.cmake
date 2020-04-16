BRAINSia/BRAINSTools

commit 241ec75298381cd15f305fce740b33a7c063bf9f

ENH: Improve setting of -march and -mtune for superbuild.

Common.cmake
@@ -298,23 +298,80 @@ endif()
 option(BUILD_COVERAGE "Build ${LOCAL_PROJECT_NAME} for coverage analysis" OFF)
 mark_as_advanced(BUILD_COVERAGE)
 
+# ------ duplicate from ITK
+function(check_compiler_optimization_flags c_optimization_flags_var cxx_optimization_flags_var)
+  set(${c_optimization_flags_var} "" PARENT_SCOPE)
+  set(${cxx_optimization_flags_var} "" PARENT_SCOPE)
+
+  if(MSVC)
+    check_avx_flags(InstructionSetOptimizationFlags)
+    if("${CMAKE_SIZEOF_VOID_P}" EQUAL "4")
+       list(APPEND InstructionSetOptimizationFlags
+            /arch:SSE /arch:SSE2)
+    endif()
+  elseif(NOT EMSCRIPTEN)
+    if (${CMAKE_C_COMPILER} MATCHES "icc.*$")
+      set(USING_INTEL_ICC_COMPILER TRUE)
+    endif()
+    if (${CMAKE_CXX_COMPILER} MATCHES "icpc.*$")
+      set(USING_INTEL_ICC_COMPILER TRUE)
+    endif()
+    if(USING_INTEL_ICC_COMPILER)
+      set(InstructionSetOptimizationFlags "")
+    else()
+      set(InstructionSetOptimizationFlags "")
+    endif ()
+
+    # Check this list on C compiler only
+    set(c_flags "")
+
+    # Check this list on C++ compiler only
+    set(cxx_flags "")
+
+    # Check this list on both C and C++ compilers
+    set(InstructionSetOptimizationFlags
+       # https://gcc.gnu.org/onlinedocs/gcc-4.8.0/gcc/i386-and-x86_002d64-Options.html
+       # NOTE the corei7 release date was 2008
+       -mtune=native # Tune the code for the computer used compile ITK, but allow running on generic cpu archetectures
+       ## use corei7-avx for ARGON cluster builds
+       -march=native # Use ABI settings to support corei7 (circa 2008 ABI feature sets, corei7-avx circa 2013)
+       )
+  endif()
+  set(c_and_cxx_flags ${InstructionSetOptimizationFlags})
+
+  check_c_compiler_flags(    CMAKE_C_WARNING_FLAGS ${c_and_cxx_flags} ${c_flags})
+  check_cxx_compiler_flags(CMAKE_CXX_WARNING_FLAGS ${c_and_cxx_flags} ${cxx_flags})
+
+  set(${c_optimization_flags_var} "${CMAKE_C_WARNING_FLAGS}" PARENT_SCOPE)
+  set(${cxx_optimization_flags_var} "${CMAKE_CXX_WARNING_FLAGS}" PARENT_SCOPE)
+endfunction()
+if(NOT BRAINSToools_C_OPTIMIZATION_FLAGS OR NOT BRAINSToools_CXX_OPTIMIZATION_FLAGS ) # Only check once if not explicitly set on command line
+  #-----------------------------------------------------------------------------
+  #Check the set of warning flags the compiler supports
+  check_compiler_optimization_flags(C_OPTIMIZATION_FLAGS CXX_OPTIMIZATION_FLAGS)
+endif()
+if(NOT BRAINSToools_C_OPTIMIZATION_FLAGS) #Not set on cmake command line option -DBRAINSToools_C_OPTIMIZATION_FLAGS:STRING=""
+  set(BRAINSToools_C_OPTIMIZATION_FLAGS ${C_OPTIMIZATION_FLAGS} CACHE STRING "ITK C Compiler ABI/Optimization flags, Use '-march=native' to maximize performance, but break portabilitly.")
+else()
+  set(BRAINSToools_C_OPTIMIZATION_FLAGS ${BRAINSToools_C_OPTIMIZATION_FLAGS} CACHE STRING "ITK C Compiler ABI/Optimization flags, Use '-march=native' to maximize performance, but break portabilitly.")
+endif()
+if(NOT BRAINSToools_CXX_OPTIMIZATION_FLAGS) #Not set on cmake command line option -DBRAINSToools_CXX_OPTIMIZATION_FLAGS:STRING=""
+  set(BRAINSToools_CXX_OPTIMIZATION_FLAGS ${CXX_OPTIMIZATION_FLAGS} CACHE STRING "ITK CXX Compiler ABI/Optimization flags, Use '-march=native' to maximize performance, but break portabilitly.")
+else()
+  set(BRAINSToools_CXX_OPTIMIZATION_FLAGS ${BRAINSToools_CXX_OPTIMIZATION_FLAGS} CACHE STRING "ITK CXX Compiler ABI/Optimization flags, Use '-march=native' to maximize performance, but break portabilitly.")
+endif()
+mark_as_advanced(BRAINSToools_CXX_OPTIMIZATION_FLAGS)
+mark_as_advanced(BRAINSToools_C_OPTIMIZATION_FLAGS)
+unset(C_OPTIMIZATION_FLAGS)
+unset(CXX_OPTIMIZATION_FLAGS)
+
 option(BUILD_OPTIMIZED "Set compiler flags for native host building." OFF)
 mark_as_advanced(BUILD_OPTIMIZED)
 
 if(BUILD_OPTIMIZED AND NOT BUILD_COVERAGE)
-  include(CheckCXXCompilerFlag)
-  include(CheckCCompilerFlag)
-  CHECK_CXX_COMPILER_FLAG("-march=native" CAN_BUILD_CXX_OPTIMIZED)
-  CHECK_C_COMPILER_FLAG("-march=native" CAN_BUILD_C_OPTIMIZED)
   if(CAN_BUILD_CXX_OPTIMIZED AND CAN_BUILD_C_OPTIMIZED)
-    string(FIND CMAKE_CXX_FLAGS "-march=native" PREVIOUS_CXX_OPTIMIZED_SET)
-    if(PREVIOUS_CXX_OPTIMIZED_SET LESS 0)
-      string(APPEND CMAKE_CXX_FLAGS " -march=native")
-    endif()
-    string(FIND CMAKE_C_FLAGS "-march=native" PREVIOUS_C_OPTIMIZED_SET)
-    if(PREVIOUS_C_OPTIMIZED_SET LESS 0 )
-      string(APPEND CMAKE_C_FLAGS " -march=native")
-    endif()
+    string(APPEND CMAKE_CXX_FLAGS ${BRAINSToools_CXX_OPTIMIZATION_FLAGS})
+    string(APPEND CMAKE_C_FLAGS ${BRAINSToools_CXX_OPTIMIZATION_FLAGS})
     string(REPLACE " " ";" CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${${PROJECT_NAME}_C_OPTIMIZATION_FLAGS} ${${PROJECT_NAME}_C_WARNING_FLAGS}")
     list(REMOVE_DUPLICATES CMAKE_C_FLAGS)
     string(REPLACE ";" " " CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")

SuperBuild/External_ITKv5.cmake
@@ -121,6 +121,8 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       -DITKV3_COMPATIBILITY:BOOL=OFF
       -DITK_BUILD_DEFAULT_MODULES:BOOL=ON
       -DITK_INSTALL_NO_DEVELOPMENT:BOOL=ON
+      -DITK_CXX_OPTIMIZATION_FLAGS:STRING=${BRAINSToools_CXX_OPTIMIZATION_FLAGS}
+      -DITK_C_OPTIMIZATION_FLAGS:STRING=${BRAINSToools_C_OPTIMIZATION_FLAGS}
       -DModule_AnisotropicDiffusionLBR:BOOL=ON
       -DModule_GenericLabelInterpolator:BOOL=ON # Needed for ANTs
       -DModule_ITKReview:BOOL=ON

commit 7de00e91993aea7562a749afb2199ff1a360eac6

COMP: Fix windows compilation adding missing check_avx_flags function
This commit fixes the following configuration error introduced
in 241ec75 (ENH: Improve setting of -march and -mtune for superbuild):

  CMake Error at BRAINSTools/Common.cmake:307 (check_avx_flags):
      Unknown CMake command "check_avx_flags".
    Call Stack (most recent call first):
      BRAINSTools/Common.cmake:351 (check_compiler_optimization_flags)
      BRAINSTools/CMakeLists.txt:86 (include)

Common.cmake
@@ -299,6 +299,78 @@ option(BUILD_COVERAGE "Build ${LOCAL_PROJECT_NAME} for coverage analysis" OFF)
 mark_as_advanced(BUILD_COVERAGE)
 
 # ------ duplicate from ITK
+# Check for the presence of AVX and figure out the flags to use for it.
+# Adapted from https://gist.github.com/UnaNancyOwen/263c243ae1e05a2f9d0e
+function(check_avx_flags avx_flags_var)
+  set(avx_flags_var)
+
+  include(CheckCXXSourceRuns)
+  set(_safe_cmake_required_flags "${CMAKE_REQUIRED_FLAGS}")
+  set(CMAKE_REQUIRED_FLAGS)
+
+  # Check AVX
+  if(MSVC_VERSION GREATER_EQUAL 1600)
+    set(CMAKE_REQUIRED_FLAGS "/arch:AVX") # set flags to be used in check_cxx_source_runs below
+  endif()
+  check_cxx_source_runs("
+    #include <immintrin.h>
+    int main()
+    {
+      __m256 a, b, c;
+      const float src[8] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f };
+      float dst[8];
+      a = _mm256_loadu_ps( src );
+      b = _mm256_loadu_ps( src );
+      c = _mm256_add_ps( a, b );
+      _mm256_storeu_ps( dst, c );
+
+      for( int i = 0; i < 8; i++ ){
+        if( ( src[i] + src[i] ) != dst[i] ){
+          return -1;
+        }
+      }
+
+      return 0;
+    }"
+    have_avx_extensions_var)
+
+  # Check AVX2
+  if(MSVC_VERSION GREATER_EQUAL 1800)
+    set(CMAKE_REQUIRED_FLAGS "/arch:AVX2") # set flags to be used in check_cxx_source_runs below
+  endif()
+  check_cxx_source_runs("
+    #include <immintrin.h>
+    int main()
+    {
+      __m256i a, b, c;
+      const int src[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+      int dst[8];
+      a =  _mm256_loadu_si256( (__m256i*)src );
+      b =  _mm256_loadu_si256( (__m256i*)src );
+      c = _mm256_add_epi32( a, b );
+      _mm256_storeu_si256( (__m256i*)dst, c );
+
+      for( int i = 0; i < 8; i++ ){
+        if( ( src[i] + src[i] ) != dst[i] ){
+          return -1;
+        }
+      }
+
+      return 0;
+    }"
+    have_avx2_extensions_var)
+
+  set(CMAKE_REQUIRED_FLAGS "${_safe_cmake_required_flags}")
+
+  # Set Flags
+  if(have_avx2_extensions_var AND MSVC_VERSION GREATER_EQUAL 1800)
+    set(avx_flags_var "${avx_flags_var} /arch:AVX2")
+  elseif(have_avx_extensions_var AND MSVC_VERSION GREATER_EQUAL 1600)
+    set(avx_flags_var "${avx_flags_var} /arch:AVX")
+  endif()
+endfunction()
+
+# ------ duplicate from ITK
 function(check_compiler_optimization_flags c_optimization_flags_var cxx_optimization_flags_var)
   set(${c_optimization_flags_var} "" PARENT_SCOPE)
   set(${cxx_optimization_flags_var} "" PARENT_SCOPE)
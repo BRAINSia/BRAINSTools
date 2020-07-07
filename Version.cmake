3cf16800c2b618f4d4fe16e63e8c9dc68dabef1b
Hans Johnson <hans-johnson@uiowa.edu>
2020-07-07 18:08:09 -0500

    ENH: Update to version 5.3.1 with installed shared libs.

--- Version.cmake
+++ Version.cmake
@@ -11,10 +11,10 @@ include(ProjectSourceVersion)
 
 ## NOTE:    NEED TO MANUALLY UPDATE ALL XML FILES
 ##          vim $(git grep  -l "<version>" -- *.xml)
-##          :%s/< *version *> *[^<]*</<version>5.3.0</g
+##          :%s/< *version *> *[^<]*</<version>5.3.1</g
 ##          git commit
-##          git tag -a v5.3.0
-##          git push origin v5.3.0
+##          git tag -a v5.3.1
+##          git push origin v5.3.1
 ##          git push origin --tags  ## DON'T forget to do this!
 ## See note above for updating versions
 #-- DON'T OVERRIDE projcet() set(${PROJECT_NAME}_VERSION_MAJOR ${_GIT_VERSION_MAJOR})

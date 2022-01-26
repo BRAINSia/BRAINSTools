37b0a0f64cbff2b987fee321c9363554cee52b14
Hans Johnson <hans-johnson@uiowa.edu>
2022-01-26 14:09:41 -0600

    BUG: Fix SetupForDevelopment.sh
    Remove setting up Autoworkup automatically.

--- Utilities/SetupForDevelopment.sh
+++ Utilities/SetupForDevelopment.sh
@@ -2,7 +2,8 @@
 
 $(dirname "$0")/DevelopmentSetupScripts/SetupHooks.sh
 ## Add ssh commit reference
-git remote add bia git@github.com:BRAINSia/BRAINSTools.git
+git remote -v | grep bia || git remote add bia git@github.com:BRAINSia/BRAINSTools.git 2>&1 || true
+git fetch bia
 ## run pre-commit for python dir AutoWorkup
-pip install -r $(dirname "$0")/../AutoWorkup/requirements-dev.txt
-pre-commit install
+#pip install -r $(dirname "$0")/../AutoWorkup/requirements-dev.txt
+#pre-commit install
